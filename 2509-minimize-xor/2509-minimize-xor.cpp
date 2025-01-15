0396459b2d8f10d0deb2c9a5b3b03149f15b6196
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-01-15 08:09:55 +0530

    Time: 2 ms (7.01%), Space: 8.1 MB (40.19%) - LeetHub

--- 2509-minimize-xor/2509-minimize-xor.cpp
+++ 2509-minimize-xor/2509-minimize-xor.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int minimizeXor(int num1, int num2) {
-        int count=0;
-        while(num2){
-            if(num2&1) count+=1;
-            num2=num2>>1;
-        }
+        // int count=0;
+        // while(num2){
+        //     if(num2&1) count+=1;
+        //     num2=num2>>1;
+        // }
+        int count=__builtin_popcount(num2);
           cout<<count<<endl;
       int x=0;
       for(int i=30;i>=0;i--){
