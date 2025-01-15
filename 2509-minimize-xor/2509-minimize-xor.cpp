9cf3e8b79be9e1abcdf68c642f06ebdebbf0b06b
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-01-15 07:56:23 +0530

    Time: 3 ms (5.14%), Space: 7.9 MB (64.02%) - LeetHub

---
+++ 2509-minimize-xor/2509-minimize-xor.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        int count=0;
+        while(num2){
+            if(num2&1) count+=1;
+            num2=num2>>1;
+        }
+          cout<<count<<endl;
+      int x=0;
+      for(int i=30;i>=0;i--){
+   if(num1&(1<<i) && count){
+    
+        x=x|(1<<i);
+        count--;
+
+   }
+  
+      }
+       cout<<count<<endl;
+   for(int i=0;i<30;i++){
+    if((x&(1<<i))==0 && count){
+        x=x|(1<<i);
+        count--;
+    }
+   }
+      cout<<x<<endl;
+      
+        return x;
+    }
+};
