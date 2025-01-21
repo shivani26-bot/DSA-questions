72139692447a0e7e267e77273b7a3e7b4ad3ab1f
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-01-21 08:46:17 +0530

    Time: 3 ms (36.22%), Space: 13.3 MB (33.17%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,64 @@
+class DisjointSet{
+vector<int>rank, parent, size;
+public:
+    DisjointSet(int n){
+        rank.resize(n+1,0), size.resize(n+1,1),parent.resize(n+1);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+    }
+    int findUPar(int node){
+        if(node==parent[node]) return node;
+        return parent[node]=findUPar(parent[node]);
+    }
+    void unionByRank(int u,int v){
+        int ulp_u=findUPar(u);
+        int ulp_v=findUPar(v);
+        if(ulp_u==ulp_v) return ;
+        if(rank[ulp_u]<rank[ulp_v]){
+            parent[ulp_u]=ulp_v;
+        }
+        else if(rank[ulp_u]>rank[ulp_v]){
+            parent[ulp_v]=ulp_u;
+        }
+        else{
+              parent[ulp_v]=ulp_u;
+              rank[ulp_u]++;
+        }
+    }
+      void unionBySize(int u,int v){
+        int ulp_u=findUPar(u);
+        int ulp_v=findUPar(v);
+        if(ulp_u==ulp_v) return ;
+        if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=ulp_u;
+        }
+        else{
+              parent[ulp_v]=ulp_u;
+             size[ulp_u]+=ulp_v;
+        }
+    }
+
+};
+class Solution {
+public:
+// union find
+// union will merge 2 disjoint sets
+// find will find the set to which node belongs
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+     int n=edges.size();
+       DisjointSet ds(n);
+        for(auto it:edges){
+            int u=it[0];
+            int v=it[1];
+            if(ds.findUPar(u)==ds.findUPar(v)){
+                return {u,v};
+            }
+            else{
+                ds.unionByRank(u,v);
+            }
+        }
+        return {-1,-1};
+    }
+};
