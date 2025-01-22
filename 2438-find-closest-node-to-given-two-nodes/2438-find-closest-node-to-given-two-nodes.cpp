53beeba6d12ce109ed95278ee38fecfc3121aa7d
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-01-23 05:05:50 +0530

    Time: 9 ms (88.6%), Space: 105.4 MB (49.46%) - LeetHub

--- 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
@@ -5,14 +5,40 @@ public:
 // dist1=  inf inf inf inf
 // node1=0  0  inf  1   2
 // node2=1 inf  0   1   2
-int n;
-void dfs(int node, vector<int>&edges,vector<int>&distance,vector<bool>&visited){
-visited[node]=true;
-int v=edges[node];
-if(v!=-1 && !visited[v]){
 
-    distance[v]=1+distance[node];
-    dfs(v,edges,distance,visited);
+// when we get dist1,dist2 for each index find the maximum distance of two and store it, and finally find the mininmum of all those maximum distances
+
+int n;
+// void dfs(int node, vector<int>&edges,vector<int>&distance,vector<bool>&visited){
+// visited[node]=true;
+// int v=edges[node];
+// if(v!=-1 && !visited[v]){
+// visited[v]=true;
+//     distance[v]=1+distance[node];
+//     dfs(v,edges,distance,visited);
+// }
+// }
+void bfs(int node, vector<int>&edges,vector<int>&distance,vector<bool>&visited){
+// visited[node]=true;
+// int v=edges[node];
+// if(v!=-1 && !visited[v]){
+
+//     distance[v]=1+distance[node];
+//     dfs(v,edges,distance,visited);
+// }
+
+queue<int>q;
+q.push(node);
+visited[node]=true;
+while(!q.empty()){
+    int n=q.front();
+    q.pop();
+    int adjNode=edges[n];
+    if(adjNode!=-1 && !visited[adjNode] ){
+        distance[adjNode]=1+distance[n];
+        q.push(adjNode);
+        visited[adjNode]=true;
+    }
 }
 }
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
@@ -26,9 +52,13 @@ dist1[node1]=0;
 dist2[node2]=0;
 
 
-      dfs(node1,edges,dist1,visited1);
+    //   dfs(node1,edges,dist1,visited1);
        
-      dfs(node2,edges,dist2,visited2);
+    //   dfs(node2,edges,dist2,visited2);
+
+    bfs(node1,edges,dist1,visited1);
+       
+      bfs(node2,edges,dist2,visited2);
 int minDistanceNode=-1;
 int minDistanceTillNow=INT_MAX;
       for(int i=0;i<n;i++){
