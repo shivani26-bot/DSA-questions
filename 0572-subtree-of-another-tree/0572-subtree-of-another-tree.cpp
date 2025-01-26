b28c43b4241f7dc3b94bad4155fe7a1f69a6de95
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-01-26 11:11:14 +0530

    Time: 0 ms (100%), Space: 28.9 MB (53.6%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -11,44 +11,19 @@
  */
 class Solution {
 public:
-// bool areSame(TreeNode* r1, TreeNode* r2){
-//  if(!r1 && !r2) return true;
-//         if(!r1 || !r2) return false;
-//        return r1->val == r2->val && areSame(r1->left,r2->left) && areSame(r1->right,r2->right);
-
-// }
-void preorderTraversal(TreeNode* root, vector<int>& array){
-          if(!root){
-        array.push_back(INT_MIN);
-        return;
-        }
-        array.push_back(root->val);
-        preorderTraversal(root->left,array);
-        preorderTraversal(root->right,array);
-    return;
-
+bool check(TreeNode* root, TreeNode* subRoot){
+     if(!root && !subRoot) return true;
+        if(!root || !subRoot) return false;
+        return (root->val== subRoot->val && check(root->left,subRoot->left) && check(root->right,subRoot->right));
 }
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        //    if(!root) return false;
-        //  if(areSame(root,subRoot)){
-        //      return true;
-        //  }
-      
-        // return  isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot);
+        if(!root) return false;
+       if(check(root,subRoot)) return true;
 
-        vector<int>preorderArray1,preorderArray2;
-        preorderTraversal(root,preorderArray1);
-        preorderTraversal(subRoot,preorderArray2);
-        if(preorderArray1.size()<preorderArray2.size()) return false;
-        for(int i=0;i<=preorderArray1.size()-preorderArray2.size();i++){
-            int c=0;
-            for(int j=i;j<i+preorderArray2.size();j++){
-                if(preorderArray1[j]==preorderArray2[j-i]) c++;
-            }
-              if(c==preorderArray2.size()) return true;
-        }
-      return false;
+bool lst=isSubtree(root->left,subRoot) ;
+ bool rst= isSubtree(root->right,subRoot);
+ return lst || rst;
+       
+
     }
-        
-    
 };
