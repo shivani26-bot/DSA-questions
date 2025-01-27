6faa47fdd8a9c913801ee24a77ed606a68c60899
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-01-27 07:12:11 +0530

    Time: 0 ms (100%), Space: 21.4 MB (94.67%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -9,6 +9,40 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//  class Solution {
+// public:
+//     bool hasPathSum(TreeNode* root, int targetSum) {
+
+//         if (root == NULL)
+// return false;
+// if (root->left == NULL && root->right == NULL && targetSum - root->val == 0)
+// return true;
+// return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+        
+//     }
+// };
+//  class Solution {
+// public:
+// bool rootToLeafSum(TreeNode* root, int sum, int targetSum){
+//   if(!root) return false;
+
+//   sum+=root->val;
+// if(!root->left && !root->right && sum==targetSum){
+//     return true;
+// }
+//  bool lst= rootToLeafSum(root->left,sum,targetSum);
+//  bool rst= rootToLeafSum(root->right,sum,targetSum);
+
+//  return lst||rst;
+// }
+//     bool hasPathSum(TreeNode* root, int targetSum) {
+//          if (!root)
+//         return false;
+    
+//         int sum=0;
+//       return  rootToLeafSum(root,sum,targetSum);
+//     }
+// };
 class Solution {
 public:
 bool pathSum(TreeNode* root, int targetSum,int &sum){
