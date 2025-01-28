b8698d353b49b5c254e3ee92c3b5797f9a872370
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-01-28 08:16:25 +0530

    Time: 0 ms (100%), Space: 12.5 MB (60.72%) - LeetHub

--- 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.cpp
+++ 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.cpp
@@ -9,21 +9,39 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //traversing the bst in inorder we will get the sorted order of nodes
 class Solution {
 public:
-void dfs(TreeNode* root, TreeNode*& prev, int& result){
+// void dfs(TreeNode* root, TreeNode*& prev, int& result){
+// if(!root) return;
+//     dfs(root->left,prev,result);
+//      if(prev!=NULL)
+//     result=min(result, abs(root->val-prev->val));
+//     prev=root;
+//     dfs(root->right, prev, result);
+   
+// }
+//     int minDiffInBST(TreeNode* root) {
+//         int result=INT_MAX;
+//         TreeNode* prev=NULL;
+//         dfs(root,prev,result);
+//         return result;
+//     }
+
+void dfs(TreeNode* root, vector<int>& arr){
 if(!root) return;
-    dfs(root->left,prev,result);
-     if(prev!=NULL)
-    result=min(result, abs(root->val-prev->val));
-    prev=root;
-    dfs(root->right, prev, result);
+    dfs(root->left,arr);
+   arr.push_back(root->val);
+    dfs(root->right, arr);
    
 }
     int minDiffInBST(TreeNode* root) {
-        int result=INT_MAX;
-        TreeNode* prev=NULL;
-        dfs(root,prev,result);
+        vector<int>arr;
+int result=INT_MAX;
+        dfs(root,arr);
+        for(int i=0;i<arr.size()-1;i++){
+            result=min(result, abs(arr[i]-arr[i+1]));
+        }
         return result;
     }
 };
