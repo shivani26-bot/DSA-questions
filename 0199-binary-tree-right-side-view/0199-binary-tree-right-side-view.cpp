fc2cb4908dc5e2be2be8949e31ee6c1df85a438b
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-01-31 10:51:20 +0530

    Time: 4 ms (2.68%), Space: 14.9 MB (63.11%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -9,31 +9,68 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//  class Solution {
+// public:
+// void rightView(TreeNode* root, int level, int &maxLevel , vector<int>&ans){
+//     if(!root) return ;
+//     if(level>maxLevel){
+//         maxLevel=level;
+//         ans.push_back(root->val);
+//     }
+//     rightView(root->right,level+1,maxLevel,ans);
+//     rightView(root->left,level+1,maxLevel,ans);
+// }
+//     vector<int> rightSideView(TreeNode* root) {
+        
+//         int maxLevel=-1;
+//    int level=0;
+//    vector<int>ans;
+//    rightView(root,level,maxLevel,ans);
+//    return ans;
+//     }
+// };
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
-        if(!root) return {};
-        queue<TreeNode*>q;
-        q.push(root);
-        vector<int>ans;
+//     vector<int> rightSideView(TreeNode* root) {
+//         if(!root) return {};
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         vector<int>ans;
 
-        while(!q.empty()){
-             int size= q.size();
-            TreeNode* node=NULL;
-             while(size--){
-                 node=q.front();
-q.pop();
- if(node->left) {
-                    q.push(node->left);
-                }
+//         while(!q.empty()){
+//              int size= q.size();
+//             TreeNode* node=NULL;
+//              while(size--){
+//                  node=q.front();
+// q.pop();
+//  if(node->left) {
+//                     q.push(node->left);
+//                 }
                
-                 if(node->right) {
-                    q.push(node->right);              
-                }
+//                  if(node->right) {
+//                     q.push(node->right);              
+//                 }
                 
-             }
-             ans.push_back(node->val);
-        }
-        return ans;
+//              }
+//              ans.push_back(node->val);
+//         }
+//         return ans;
+//     }
+
+// dfs preorder traversal
+void dfs(TreeNode* root, int level,vector<int>&ans){
+if(!root) return;
+
+    if(ans.size()<level){
+        ans.push_back(root->val);
+    }
+    dfs(root->right,level+1,ans);
+    dfs(root->left,level+1,ans);
+}
+ vector<int> rightSideView(TreeNode* root) {
+     vector<int>ans;
+     dfs(root,1,ans);
+     return ans;
     }
 };
