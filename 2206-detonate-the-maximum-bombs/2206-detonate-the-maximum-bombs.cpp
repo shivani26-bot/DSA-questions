992174290ea4c3385d24becf9e178ef7900a212f
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-02-03 11:10:34 +0530

    Time: 180 ms (26.17%), Space: 35.4 MB (27.52%) - LeetHub

---
+++ 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+// if radius of a bomb1 is >= distance between 2 bombs ie bomb1 and bomb 2
+// distance between 2 bombs= sqrt((x1-x2)^2 +(y1-y2)^2 )
+// then only bomb 1 will cause bomb 2 to explode
+// r of a bomb >=d between bombs
+// d=sqrt((x1-x2)^2 +(y1-y2)^2)
+// directed graph
+// if bomb1 explodes bomb2 it's not necessary that bomb2 will explode bomb1 also
+// it depends on the range of bomb2
+//               0      1        2       3       4
+//   bombs = [[1,2,3],[2,3,1],[3,4,2],[4,5,3],[5,6,4]]
+typedef long long ll;
+void dfs(int node, unordered_set<int>&visited,vector<vector<int>>&adjList )
+    {
+         visited.insert(node);
+         for(auto it: adjList [node]){
+            if(visited.find(it)==visited.end()){
+                dfs(it,visited,adjList);
+            }
+         }
+    }
+    
+    int maximumDetonation(vector<vector<int>>& bombs) {
+        int n=bombs.size();
+vector<vector<int>>adjList(n);
+        // make adjacency list 
+        // check krna hai i ke fatne se jth bomb fatega ya nahi 
+        for(int b1=0;b1<n;b1++){
+            for(int b2=0;b2<n;b2++){
+                if(b1==b2) continue;
+                ll b1_x1= bombs[b1][0];
+                ll b1_y1=  bombs[b1][1];
+                ll b1_r1=  bombs[b1][2];
+                ll b2_x2=  bombs[b2][0];
+                ll b2_y2=  bombs[b2][1];
+                ll b2_r2=  bombs[b2][2];
+                ll u=(b2_x2-b1_x1)*(b2_x2-b1_x1);
+                ll v=(b2_y2-b1_y1)*(b2_y2-b1_y1);
+                // ll distance_between_b1_b2= sqrt(u+v);
+                // sqrt() returns a floating-point number, but you're storing it in an ll, which can cause precision issues.
+                 ll distance_between_b1_b2= u+v;
+                if(ll(b1_r1*b1_r1)>=distance_between_b1_b2)
+                  adjList[b1].push_back(b2);
+            }
+        }
+        int result=0;
+        unordered_set<int>visited;
+        for(int i=0;i<n;i++){
+            dfs(i,visited,adjList);
+            int count=visited.size();
+            result=max(result,count);
+            visited.clear();
+        }
+        return result;
+    }
+};
