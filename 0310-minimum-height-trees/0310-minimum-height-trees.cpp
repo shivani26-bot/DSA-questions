6310232ebdeda130c183849536ce0a8152d7c611
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-02-04 09:16:28 +0530

    Time: 84 ms (22.19%), Space: 69 MB (20.91%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -9,49 +9,91 @@ public:
     // 2)remove the leaf nodes(degree 1) keep track of degree while building the graph , as we remove the leaf with degree 1 , its neighbour will also reduce the degree.keep on doing the process while(number of nodes >2)
     // if in last either 2 nodes remain or 1 node remain that is the answer.
     
-vector<int>res;
-if(n<=0) return res;
-    if(n==1) {
-        res.push_back(0);
-        return res;
+// vector<int>res;
+// if(n<=0) return res;
+//     if(n==1) {
+//         res.push_back(0);
+//         return res;
+//     }
+//     vector<int>degree(n,0);
+//     vector<vector<int>>adjList(n);
+//     for(auto edge: edges){
+//         degree[edge[0]]++;
+//         degree[edge[1]]++;
+//         adjList[edge[0]].push_back(edge[1]);
+//         adjList[edge[1]].push_back(edge[0]);
+//     }
+//  queue<int>q;
+//     for(int i=0;i<n;i++){
+//         if(degree[i]==1){
+//            q.push(i); 
+//         }
+//     }
+//     // we go level by level here and try to remove leaf nodes , hence use bfs
+// while(n>2){
+// int sz= q.size();
+// n-=sz;
+// while(sz>0){
+//     int ele= q.front();
+//     q.pop();
+//     //go on all the neighbors and reduce their degree
+//     for(auto e: adjList[ele]){
+//         degree[e]--;
+//         if(degree[e]==1) q.push(e);
+//     }
+//     sz--;
+// }
+// }
+//       while(!q.empty()){
+//         int el= q.front();
+//         q.pop();
+//         res.push_back(el);
+//       }
+//       return res;
+//     }
+vector<int>ans;
+if(n==1){
+    if(edges.size()==0) ans.push_back(0);
+    return ans;
+}
+unordered_map<int,vector<int>>adjList;
+vector<int>degree(n,0);
+for(auto it: edges){
+    int u=it[0];
+    int v=it[1];
+    adjList[u].push_back(v);
+    adjList[v].push_back(u);
+    degree[u]++;
+    degree[v]++;
+}
+queue<int>q;
+for(int i=0;i<n;i++){
+    if(degree[i]==1){
+        q.push(i);
     }
-    vector<int>degree(n,0);
-    vector<vector<int>>adjList(n);
-    for(auto edge: edges){
-        degree[edge[0]]++;
-        degree[edge[1]]++;
-        adjList[edge[0]].push_back(edge[1]);
-        adjList[edge[1]].push_back(edge[0]);
-    }
- queue<int>q;
-    for(int i=0;i<n;i++){
-        if(degree[i]==1){
-           q.push(i); 
+}
+while(n>2){
+    int size=q.size();
+    n-=size;
+    while(size--){
+
+        int ele=q.front();
+        q.pop();
+        for(auto it: adjList[ele]){
+            degree[it]--;
+            if(degree[it]==1) q.push(it);
         }
     }
-    // we go level by level here and try to remove leaf nodes , hence use bfs
-while(n>2){
-int sz= q.size();
-n-=sz;
-while(sz>0){
-    int ele= q.front();
-    q.pop();
-    //go on all the neighbors and reduce their degree
-    for(auto e: adjList[ele]){
-        degree[e]--;
-        if(degree[e]==1) q.push(e);
-    }
-    sz--;
 }
-}
-      while(!q.empty()){
-        int el= q.front();
-        q.pop();
-        res.push_back(el);
-      }
-      return res;
-    }
 
+
+while(!q.empty()){
+    int ele=q.front();
+    q.pop();
+    ans.push_back(ele);
+}
+return ans;
+    }
     // time: o(n)
     // space: o(n)
 };
