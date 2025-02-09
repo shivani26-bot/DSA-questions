23d19749da83c671751f31f18f1dd0ec6e539083
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-02-09 18:09:55 +0530

    Time: 0 ms (100%), Space: 13.7 MB (57.55%) - LeetHub

--- 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.cpp
+++ 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.cpp
@@ -12,41 +12,54 @@
 class Solution {
 public:
 
-bool bfs(TreeNode* root){
-    queue<TreeNode*>q;
-    q.push(root);
-      bool foundNull=false;
-    while(!q.empty()){
-      TreeNode* node= q.front();
-      q.pop();
+// bool bfs(TreeNode* root){
+//     queue<TreeNode*>q;
+//     q.push(root);
+//       bool foundNull=false;
+//     while(!q.empty()){
+//       TreeNode* node= q.front();
+//       q.pop();
 
-      if(!node) {
-        foundNull=true; //if node is null mark it true
-      }
-      else{ //if node is present then check whether have seen null in past or not if yes then return false other wise push the left and right child 
-        if(foundNull) return false;
-        q.push(node->left);
-        q.push(node->right);// if there is null node it pushes 0, if there is node it pushes the address of the node
-        // cout<<node->left<<" "<<node->right;
-      }
+//       if(!node) {
+//         foundNull=true; //if node is null mark it true
+//       }
+//       else{ //if node is present then check whether have seen null in past or not if yes then return false other wise push the left and right child 
+//         if(foundNull) return false;
+//         q.push(node->left);
+//         q.push(node->right);// if there is null node it pushes 0, if there is node it pushes the address of the node
+//         // cout<<node->left<<" "<<node->right;
+//       }
         
-    }
-    return true;
+//     }
+//     return true;
+// }
+
+//     bool isCompleteTree(TreeNode* root) {
+
+// //  if at last level we find null then after that null there should be no elements after it
+
+// // if any node has seen null in the past it means it's not a complete binary tree
+
+// return bfs(root);
+
+
+//     }
+int countNodes(TreeNode* root){
+    if(!root) return false;
+    return 1+countNodes(root->left)+countNodes(root->right);
 }
-int height(TreeNode* root)
-{
-  if(!root) return 0;
-  int lst= height(root->left);
-  int rst= height(root->right);
-  return 1+max(lst,rst);
+bool dfs(TreeNode* root, int i, int totalNodes){
+
+    if(!root) return true;
+    if(i>totalNodes) return false;
+    return dfs(root->left, 2*i,totalNodes) && dfs(root->right,2*i+1,totalNodes);
 }
     bool isCompleteTree(TreeNode* root) {
+//  represent tree in array, if index  > number of nodes in a tree then it will not be a complete binary tree, if it's a complete binary tree we will never get the gap in between nodes, count total number of nodes, then run dfs for root->left and root->right, store elements in root->left to 2*i index of array and root->right to 2*i+1 index
 
-//  if at last level we find null then after that null there should be no elements after it
-
-// if any node has seen null in the past it means it's not a complete binary tree
-
-return bfs(root);
+int totalNodes= countNodes(root);
+int i=1;
+return dfs(root,i,totalNodes);
 
 
     }
