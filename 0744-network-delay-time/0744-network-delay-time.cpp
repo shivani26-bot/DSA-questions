19022d7e3081f221ad69ae9cbf8b50eec8a3756f
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-02-14 06:36:39 +0530

    Time: 95 ms (32.39%), Space: 47.9 MB (32.93%) - LeetHub

---
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+typedef pair<int,int> P;
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        // source is given as node k
+    unordered_map<int,vector<pair<int,int>>>adjList;
+    priority_queue<P,vector<P>, greater<P>>pq;
+    for(auto it:times){
+          int u=it[0];
+          int v=it[1];
+          int time=it[2];
+          adjList[u].push_back({v,time});
+    }
+    int src=k;
+    vector<int>distance(n+1,1e9);
+    distance[src]=0;
+    pq.push({0,src});
+    int min_time=INT_MIN;
+    while(!pq.empty()){
+        int time=pq.top().first;
+        int node= pq.top().second;
+      
+        pq.pop();
+        for(auto it: adjList[node]){
+                int edgeTime=it.second;
+                int adjNode=it.first;
+         
+                if(time+edgeTime<distance[adjNode]){
+                    pq.push({time+edgeTime, adjNode});
+                    distance[adjNode]=time+edgeTime;
+                    
+       
+                }
+        }
+    }
+    // if(distance[n-1]==1e9) return -1;
+    for(int i=1;i<=n;i++){
+       min_time=max(min_time,distance[i]);
+    }
+    return min_time==1e9?-1:min_time;
+    }
+};
