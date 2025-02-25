57002419e717ae89a2b6ac11d085e5e8ab1d5a82
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-02-25 07:58:20 +0530

    Time: 35 ms (75.15%), Space: 77.3 MB (58.71%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,83 +1,90 @@
-struct Node{
-    Node* links[26];
-    bool flag=false;
-    bool characterExists(char ch){
-       return links[ch-'a'] != NULL;
+class Solution {
+public:
+    // string replaceWords(vector<string>& dictionary, string sentence) {
+    //     sentence+=' ';
+    //     string result="",word="";
+
+    //     set<string>st;
+    //     for(auto i: dictionary) st.insert(i);
+
+    //     for(auto it: sentence){
+             
+    //            if(it==' '){
+    //               if(result.size()>0) result+=' ';
+    //               string temp="";
+    //               for(auto i: word){
+    //                    temp+=i;
+    //               if(st.find(temp)!=st.end()) break;
+    //               }
+    //            result+=temp;
+    //           word="";
+    //         }
+    //      else   word+=it;
+    //      }
+    //     return result;
+    // }
+
+
+
+
+struct trieNode{
+trieNode* children[26];
+bool isEndOfWord;
+};
+trieNode* getTrieNode(){
+    trieNode *newNode= new trieNode();
+    newNode->isEndOfWord=false;
+    for(int i=0;i<26;i++){
+        newNode->children[i]=NULL;
     }
-    void insertCharacter(char ch, Node*node){
-        links[ch-'a']=node;
-    }
-    Node* moveNext(char ch){
-        return links[ch-'a'];
-    }
-    void setEnd(){
-        flag=true;
-    }
-    bool getFlag(){
-        return flag;
-    }
-  };
-class Trie{
-    public:
-    Node* root;
-    Trie(){
-        root=new Node();
-    }
-    void insert(string word){
-        Node* node=root;
-        for(int i=0;i<word.length();i++){
-            if(!node->characterExists(word[i]))  node->insertCharacter(word[i],new Node());
-            
-              node=node->moveNext(word[i]);
+    return newNode;
+}
+trieNode* root;
+void insertTrie(string& word){
+    trieNode* temp=root;
+    for(auto ch:word){
+        int idx=ch-'a';
+        if(temp->children[idx]==NULL){
+            temp->children[idx]= getTrieNode();
         }
-        node->setEnd();
+        temp=temp->children[idx];
     }
-    string givePrefix(string word){
-        Node* node=root;
-        for(int i=0;i<word.length();i++){
-            if(node->getFlag()){
-                return word.substr(0,i);
-            }
-            else if(!node->characterExists(word[i])){
+    temp->isEndOfWord=true;
+}
+string search(string word){
+    trieNode* temp=root;
+    for(int i=0;i<word.size();i++){
+        int idx=word[i]-'a';
+        if(!temp->children[idx]){
             return word;
-            }
-            node=node->moveNext(word[i]);
         }
-        return word;
+        temp=temp->children[idx];
+        if(temp->isEndOfWord) return word.substr(0,i+1);
+        
+    }
+    return word;
+}
+
+     string replaceWords(vector<string>& dictionary, string sentence) {
+  root= getTrieNode();
+       for(int i=0;i<dictionary.size();i++){
+        insertTrie(dictionary[i]);
+       }
+       stringstream ss(sentence);
+       string word;
+       string result;
+       while(getline(ss,word,' ')){
+        result+=search(word)+' ';
+       }
+       result.pop_back();//remove the last space
+       return result;
     }
 };
 
-class Solution {
-public:
-    string replaceWords(vector<string>& dictionary, string sentence) {
-        Trie *obj= new Trie();
-        for(int i=0;i<dictionary.size();i++){
-            obj->insert(dictionary[i]);
-        }
-     
-          sentence+= ' ';
-          vector<string>words;
-          string s;
-          for(int i=0;i<sentence.length();i++){
-              if(sentence[i]!=' '){
-                  s+=sentence[i];
-              }
-              else{
-                  words.push_back(s);
-                  s="";
-              }
-          }
-        //   for(auto& it: words){
-        //       it= obj->givePrefix(it);
-        //   }
-        for(int i=0;i<words.size();i++){
-            words[i]= obj->givePrefix(words[i]);
-        }
-          s="";
-          s+=words[0];
-          for(int i=1;i<words.size();i++){
-              s+= ' '+ words[i];
-          }
-          return s;
-    }
-};
+
+// dictionary={cat, catt, bat, rat}
+// sentence=the cattle
+// inset all the words from dictionary to trie, and mark endofword as true
+// for cat and catt
+// cattle has two prefix, cat and catt, hence we will choose prefix with 
+// shortest length ie cat
