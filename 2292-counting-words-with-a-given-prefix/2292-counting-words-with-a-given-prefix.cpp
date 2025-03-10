a29cd9ad2baca8b75bdc7daaceeac18ed18ccddd
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-03-10 07:42:35 +0530

    Time: 27 ms (5.05%), Space: 64 MB (5.08%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
@@ -1,12 +1,57 @@
 class Solution {
 public:
-    int prefixCount(vector<string>& words, string pref) {
-        int count = 0;
-    for (const string& word : words) {
-        if (word.find(pref) == 0) { 
-            count++;
-        }
+struct trieNode{
+    bool isEndOfWord;
+    trieNode* children[26];
+    int countPrefix;
+};
+trieNode* getTrieNode(){
+    trieNode* newNode= new trieNode();
+    for(int i=0;i<26;i++){
+        newNode->children[i]=NULL;
     }
-    return count;
+    newNode->isEndOfWord=false;
+    newNode->countPrefix=0;
+    return newNode;
+}
+void insert(trieNode* pCrawl, string word){
+    for(auto ch: word){
+        int idx=ch-'a';
+        if(!pCrawl->children[idx]){
+            pCrawl->children[idx]=getTrieNode();
+           
+        }
+        pCrawl=pCrawl->children[idx];
+         pCrawl->countPrefix+=1;
+    }
+    pCrawl->isEndOfWord=true;
+}
+int search(trieNode* pCrawl, string pref){
+
+
+for(auto ch: pref){
+    int idx=ch-'a';
+    if(!pCrawl->children[idx]){
+      return 0;
+    }
+    pCrawl=pCrawl->children[idx];
+}
+return pCrawl->countPrefix;
+}
+    int prefixCount(vector<string>& words, string pref) {
+    //     int count = 0;
+    // for (const string& word : words) {
+    //     if (word.find(pref) == 0) { 
+    //         count++;
+    //     }
+    // }
+    // return count;
+trieNode* root= getTrieNode();
+    for(int i=0;i<words.size();i++){
+        insert(root,words[i]);
+    }
+
+int result=search(root,pref);
+    return result;
     }
 };
