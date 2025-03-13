fe31d52c44c55286b64332414078fc3d97850777
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-03-13 09:00:01 +0530

    Time: 3 ms (8.91%), Space: 24.7 MB (7.6%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -9,6 +9,42 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+//  class Solution {
+// public:
+// void traverse(TreeNode* root,int &count,int &ans, int k){
+//     // if(!root || count>=k) return ;
+//     // traverse(root->left,count,ans,k);
+//     // count++;
+//     // if(count==k){
+//     //     ans=root->val;
+//     //     return;
+//     // }
+//     // traverse(root->right,count,ans,k);
+
+//  if(!root) return ;
+//     traverse(root->left,count,ans,k);
+//   // first compare count with k and then increment the count
+//     if(count++==k){
+ 
+//         ans=root->val;
+//         return;
+//     }
+//     traverse(root->right,count,ans,k);
+
+// }
+//     int kthSmallest(TreeNode* root, int k) {
+//     //     int count=0;
+//     //     int ans=-1;
+//     //    traverse(root,count,ans,k) ;
+//     //    return ans;
+//     int count=1;
+//         int ans=-1;
+//        traverse(root,count,ans,k) ;
+//        return ans;
+//     }
+// };
 class Solution {
 public:
 vector<int>ans;
