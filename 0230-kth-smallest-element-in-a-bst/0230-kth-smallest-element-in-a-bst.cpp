ab344b5823918efacc95c876aea62ed69c680e9e
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-03-13 08:59:15 +0530

    Time: 0 ms (100%), Space: 24.7 MB (18.23%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -11,35 +11,16 @@
  */
 class Solution {
 public:
-void traverse(TreeNode* root,int &count,int &ans, int k){
-    // if(!root || count>=k) return ;
-    // traverse(root->left,count,ans,k);
-    // count++;
-    // if(count==k){
-    //     ans=root->val;
-    //     return;
-    // }
-    // traverse(root->right,count,ans,k);
+vector<int>ans;
+void inorder(TreeNode* root){
 
- if(!root) return ;
-    traverse(root->left,count,ans,k);
-  // first compare count with k and then increment the count
-    if(count++==k){
- 
-        ans=root->val;
-        return;
-    }
-    traverse(root->right,count,ans,k);
-
+if(!root) return;
+    inorder(root->left);
+    ans.push_back(root->val);
+    inorder(root->right);
 }
     int kthSmallest(TreeNode* root, int k) {
-    //     int count=0;
-    //     int ans=-1;
-    //    traverse(root,count,ans,k) ;
-    //    return ans;
-    int count=1;
-        int ans=-1;
-       traverse(root,count,ans,k) ;
-       return ans;
+        inorder(root);
+        return ans[k-1];
     }
 };
