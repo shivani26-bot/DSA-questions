dcae23b7960396a1fedb0cda785b0ed4c09e8f8b
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-04-05 17:44:29 +0530

    Time: 0 ms (100%), Space: 16.6 MB (60.05%) - LeetHub

--- 1021-distribute-coins-in-binary-tree/1021-distribute-coins-in-binary-tree.cpp
+++ 1021-distribute-coins-in-binary-tree/1021-distribute-coins-in-binary-tree.cpp
@@ -26,8 +26,8 @@ public:
 //         moves+=extraCoins;
 //     }
 //     else{
-//         // int neededCoin =1+abs(root->val);
-//               int neededCoin =1;
+//         int neededCoin =1+abs(root->val);
+
 //         parent->val-=neededCoin;
 //         root->val=1;
 //         moves+=neededCoin;
@@ -38,18 +38,43 @@ public:
 //      return moves;  
 //     }
 
-int moves=0;
-int solve(TreeNode* root){
-    if(!root) return 0;//if there is no node then 0 coins will be returned
+// int moves=0;
+// int solve(TreeNode* root){
+//     if(!root) return 0;//if there is no node then 0 coins will be returned
 
-    int l= solve(root->left);
-    int r= solve(root->right);
-    moves+=abs(l)+abs(r);
-    return (l+r+root->val)-1; // every root should have only 1 coin and remaining it should send to its parent so whatever coins a root receives from left, right and root has subtract -1 and sent to its parent
+//     int l= solve(root->left);
+//     int r= solve(root->right);
+//     moves+=abs(l)+abs(r);
+//     return (l+r+root->val)-1; // every root should have only 1 coin and remaining it should send to its parent so whatever coins a root receives from left, right and root has subtract -1 and sent to its parent
+// }
+
+//     int distributeCoins(TreeNode* root) {
+//           solve(root);
+//           return moves;
+//     }
+
+int moves=0;
+void solve(TreeNode* root,TreeNode* parent){
+if(!root) return ;
+ solve(root->left,root);
+ solve(root->right, root);
+
+ if(root->val>=1){
+    int extraCoins= root->val-1;
+    parent->val+=extraCoins;//if extra coins are present at the child it will be added to parent
+    root->val=1;
+    moves+=extraCoins;
+ }
+ else{
+    int neededCoins=1+abs(root->val);
+    parent->val-=neededCoins;// if no extra coins are there with the child and child needs them then parent will have to give it to child hence it will be subtracted from parent
+    root->val=1;
+    moves+=neededCoins;
+ }
 }
 
     int distributeCoins(TreeNode* root) {
-          solve(root);
+          solve(root,root);
           return moves;
     }
 };
