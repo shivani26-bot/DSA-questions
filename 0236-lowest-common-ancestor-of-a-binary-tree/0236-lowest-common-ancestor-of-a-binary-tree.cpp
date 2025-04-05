960b213a4780c3134a16884349c8fd5c224d3d2d
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-04-05 19:10:20 +0530

    Time: 7 ms (93.33%), Space: 17.3 MB (68.45%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,39 +9,51 @@
  */
 class Solution {
 public:
-// void traverse(TreeNode* root, vector<TreeNode*>& path1,vector<TreeNode*>& path2,vector<TreeNode*>& temp,TreeNode*p, TreeNode* q ){
+// void solve(TreeNode* root, vector<TreeNode*>& path1,vector<TreeNode*>& path2,vector<TreeNode*>& temp, TreeNode* p, TreeNode* q){
 //     if(!root) return;
-//     temp.push_back(root);
-//     if(p->val == root->val){
-//         for(int i=0;i<temp.size();i++){
-//             path1.push_back(temp[i]);
-//         }
+// temp.push_back(root);
+// if(root->val==p->val){
+//     for(int i=0;i<temp.size();i++){
+//         path1.push_back(temp[i]);
 //     }
-//     if(q->val == root->val){
-//         for(int i=0;i<temp.size();i++){
-//             path2.push_back(temp[i]);
-//         }
-//     }
-//     traverse(root->left,path1,path2,temp,p,q);
-//     traverse(root->right,path1,path2,temp,p,q);
-//     temp.pop_back();
-
 // }
+// if(root->val==q->val){
+//     for(int i=0;i<temp.size();i++){
+//         path2.push_back(temp[i]);
+//     }
+// }
+// solve(root->left,path1,path2,temp,p,q);
+// solve(root->right,path1,path2,temp,p,q);
+// temp.pop_back();
+// }
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+// //      vector<TreeNode*>path1,path2,temp;
+// //      solve(root,path1,path2,temp,p,q);
+// //      int i=0;
+// // while(i<path1.size() && i<path2.size()){
+// //     if(path1[i]->val != path2[i]->val) break;
+// //     i++;
+// // }
+// // return path1[i-1];
+// if(!root) return NULL;
+// if(root->val==p->val ||  root->val==q->val) return root;
+// TreeNode* lst= lowestCommonAncestor(root->left,p,q);
+// TreeNode* rst= lowestCommonAncestor(root->right,p,q);
+// if(!lst) return rst;
+// if(!rst) return lst;
+// return root;
+//     }
+
+
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        // vector<TreeNode*> path1,path2,temp;
-        // traverse(root,path1,path2,temp,p,q);
-        // int i=0;
-        // while(i<path1.size() && i<path2.size()){
-        //     if(path1[i]->val != path2[i]->val) break;
-        //     i++;
-        // }
-        // return path1[i-1];
-        if(!root) return NULL;
-        if(root->val ==p->val || root->val ==q->val) return root;
-        TreeNode* lst= lowestCommonAncestor(root->left,p,q);
-        TreeNode* rst= lowestCommonAncestor(root->right,p,q);
-        if(!lst) return rst;
-        if(!rst) return lst;
-        return root;
+       
+if(!root) return NULL;
+if(root->val== p->val || root->val==q->val) return root;
+       TreeNode* lst= lowestCommonAncestor(root->left,p,q);
+       TreeNode* rst= lowestCommonAncestor(root->right,p,q);
+
+if(lst!=NULL && rst!=NULL) return root;
+ if(lst!=NULL) return lst;
+ return rst;
     }
 };
