43da8f2965feb0149b76a7f0bfc8c90d056e9408
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-04-05 17:24:44 +0530

    Time: 0 ms (100%), Space: 16.6 MB (60.05%) - LeetHub

--- 1021-distribute-coins-in-binary-tree/1021-distribute-coins-in-binary-tree.cpp
+++ 1021-distribute-coins-in-binary-tree/1021-distribute-coins-in-binary-tree.cpp
@@ -11,27 +11,45 @@
  */
 class Solution {
 public:
-int moves=0;
-void movesCount(TreeNode*root, TreeNode*parent){
-     if(!root) return;
-    movesCount(root->left,root);
-    movesCount(root->right,root);
+// time complexity=o(n)
+// space complexity=o(h)
+// int moves=0;
+// void movesCount(TreeNode*root, TreeNode*parent){
+//      if(!root) return;
+//     movesCount(root->left,root);
+//     movesCount(root->right,root);
 
-    if(root->val >=1){
-        int extraCoins= root->val -1;
-        parent->val +=extraCoins;
-        root->val =1;
-        moves+=extraCoins;
-    }
-    else{
-        int neededCoin =1+abs(root->val);
-        parent->val-=neededCoin;
-        root->val=1;
-        moves+=neededCoin;
-    }
+//     if(root->val >=1){
+//         int extraCoins= root->val -1;
+//         parent->val +=extraCoins;
+//         root->val =1;
+//         moves+=extraCoins;
+//     }
+//     else{
+//         // int neededCoin =1+abs(root->val);
+//               int neededCoin =1;
+//         parent->val-=neededCoin;
+//         root->val=1;
+//         moves+=neededCoin;
+//     }
+// }
+//     int distributeCoins(TreeNode* root) {
+//        movesCount(root,root);
+//      return moves;  
+//     }
+
+int moves=0;
+int solve(TreeNode* root){
+    if(!root) return 0;//if there is no node then 0 coins will be returned
+
+    int l= solve(root->left);
+    int r= solve(root->right);
+    moves+=abs(l)+abs(r);
+    return (l+r+root->val)-1; // every root should have only 1 coin and remaining it should send to its parent so whatever coins a root receives from left, right and root has subtract -1 and sent to its parent
 }
+
     int distributeCoins(TreeNode* root) {
-     movesCount(root,root);
-     return moves;
+          solve(root);
+          return moves;
     }
 };
