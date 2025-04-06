24de0c73e4fe689d01abbbf0ef06dac4b373a5df
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-04-06 19:03:48 +0530

    Time: 3 ms (34.44%), Space: 28.7 MB (9.61%) - LeetHub

--- 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -11,24 +11,23 @@
  */
 class Solution {
 public:
-unordered_map<int,int> mp;
-    int idx = 0;
-
- TreeNode* buildTree(int &preIndex,int postStart, int postEnd,vector<int>& preorder,vector<int>&postorder){
-     if(postStart > postEnd) return NULL;
-     TreeNode* root =new TreeNode(preorder[preIndex]);
-     preIndex++;
-     if(postStart == postEnd) return root;
-     int postIndex=mp[preorder[preIndex]];
-     root->left= buildTree(preIndex,postStart,postIndex,preorder,postorder);
-     root->right= buildTree(preIndex,postIndex+1,postEnd-1,preorder,postorder);
-     return root;
- }
+unordered_map<int,int>mp;
+TreeNode* solve(int &preIndex, int postStart, int postEnd, vector<int>& preorder, vector<int>& postorder){
+if(postStart>postEnd) return NULL; 
+    TreeNode* root= new TreeNode(preorder[preIndex]);
+preIndex++;
+if(postStart==postEnd) return root; //if there is only one node, 
+int postIndex=mp[preorder[preIndex]];
+    root->left= solve(preIndex,postStart, postIndex,preorder,postorder);
+    root->right=solve(preIndex, postIndex+1,postEnd-1,preorder,postorder);
+    return root;
+}
     TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
         for(int i=0;i<postorder.size();i++){
             mp[postorder[i]]=i;
         }
         int preIndex=0;
-      return  buildTree(preIndex,0,postorder.size()-1,preorder,postorder);
+       return solve(preIndex, 0,postorder.size()-1,preorder,postorder);
+
     }
 };
