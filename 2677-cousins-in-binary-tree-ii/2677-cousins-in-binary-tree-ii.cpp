a1263bb25f536b4092e5feb11286c2e8ee613c61
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-04-07 08:09:32 +0530

    Time: 0 ms (100%), Space: 304.6 MB (84.52%) - LeetHub

--- 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.cpp
+++ 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.cpp
@@ -11,48 +11,84 @@
  */
 class Solution {
 public:
-    TreeNode* replaceValueInTree(TreeNode* root) {
-        // store total sum of each level
-        vector<int>levelSum;
-            queue<TreeNode*>q;
-            q.push(root);
-            while(!q.empty()){
-                int size=q.size();
-                  int sum=0;
-                while(size--){
-                    TreeNode* node= q.front();
-                    q.pop();
-          sum+=node->val;
-          if(node->left) q.push(node->left);
-          if(node->right) q.push(node->right);
-                }
-                levelSum.push_back(sum);
-            }
-            // levelSum= 5, 13,18 //according to ex:1 
-            q.push(root);
-            root->val=0;
-            int lvl=1;
-            while(!q.empty()){
-                int size=q.size();
-                while(size--){
-TreeNode* node= q.front();
-q.pop();
+//     TreeNode* replaceValueInTree(TreeNode* root) {
+//         // store total sum of each level
+//         vector<int>levelSum;
+//             queue<TreeNode*>q;
+//             q.push(root);
+//             while(!q.empty()){
+//                 int size=q.size();
+//                   int sum=0;
+//                 while(size--){
+//                     TreeNode* node= q.front();
+//                     q.pop();
+//           sum+=node->val;
+//           if(node->left) q.push(node->left);
+//           if(node->right) q.push(node->right);
+//                 }
+//                 levelSum.push_back(sum);
+//             }
+//             // levelSum= 5, 13,18 //according to ex:1 
+//             q.push(root);
+//             root->val=0;
+//             int lvl=1;
+//             while(!q.empty()){
+//                 int size=q.size();
+//                 while(size--){
+// TreeNode* node= q.front();
+// q.pop();
+// cout<<node-> val<< " "; 
+// //find sibling sum of current node
+//                     int siblingSum=0;
+//                     siblingSum+= node->left !=NULL ?node->left->val : 0;
+// siblingSum+= node->right !=NULL ?node->right->val : 0;
 
-                    int siblingSum=0;
-                    siblingSum+= node->left !=NULL ?node->left->val : 0;
-siblingSum+= node->right !=NULL ?node->right->val : 0;
+// if(node->left){
+//         q.push(node->left);
+//     node->left->val=levelSum[lvl]-siblingSum; //update the node->left value
+
+// }
+// if(node->right){
+//         q.push(node->right);
+//     node->right->val=levelSum[lvl]-siblingSum;
+
+// }
+//                 }
+//                 lvl++;//after every pass increase the lvl
+//             }
+//             return root;
+//     }
+
+
+    TreeNode* replaceValueInTree(TreeNode* root) {
+int levelSum=root->val;//current level sum;
+queue<TreeNode*>q;
+q.push(root);
+
+while(!q.empty()){
+    int size=q.size();
+     int nextLevelSum=0;
+    while(size--){
+        TreeNode* node=q.front();
+        q.pop();
+        node->val=levelSum-node->val; //every node except the root will contain sibling sum now
+int siblingSum=0;
+       siblingSum+= node->left!=NULL ? node->left->val:0;
+       siblingSum+= node->right!=NULL ? node->right->val:0;
 
 if(node->left){
-    q.push(node->left);
-    node->left->val=levelSum[lvl]-siblingSum;
-}
-if(node->right){
+nextLevelSum+=node->left->val; //find the next level sum
+    node->left->val=siblingSum;//update the left value with sibling sum;
+     q.push(node->left);
+     }
+if(node->right) {
+    nextLevelSum+=node->right->val;
+      node->right->val=siblingSum;
     q.push(node->right);
-    node->right->val=levelSum[lvl]-siblingSum;
+    }
+    }
+    levelSum=nextLevelSum; //current level = next level it will be used in next level as curent level
 }
-                }
-                lvl++;
-            }
-            return root;
+return root;
     }
 };
