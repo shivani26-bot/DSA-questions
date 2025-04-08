791550cf7a3c033eb2f7484ab5e7e4e631df80d8
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-04-08 08:19:13 +0530

    Time: 4 ms (41.85%), Space: 84 MB (31.41%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
@@ -11,35 +11,64 @@
  */
 class Solution {
 public:
-    TreeNode* reverseOddLevels(TreeNode* root) {
-        queue<TreeNode*>q;
-        q.push(root);
+//     TreeNode* reverseOddLevels(TreeNode* root) {
+//         queue<TreeNode*>q;
+//         q.push(root);
        
-int level=0;
-        while(!q.empty()){
-            int sz=q.size();
-              vector<TreeNode*>vec;
-            while(sz--){
-               
-               TreeNode* node= q.front();
-               q.pop();
-               vec.push_back(node);
-               if(node->left) q.push(node->left);
-               if(node->right) q.push(node->right);
-            }
+// int level=0;
+//         while(!q.empty()){
+//             int sz=q.size();
+//               vector<TreeNode*>vec;
+//             while(sz--){
+//                TreeNode* node= q.front();
+//                q.pop();
+//                vec.push_back(node);
+//                if(node->left) q.push(node->left);
+//                if(node->right) q.push(node->right);
+//             }
 
-if(level%2==1){ //odd level
-int l=0,r=vec.size()-1;
-while(l<r){
-    int temp=vec[l]->val;
-    vec[l]->val=vec[r]->val;
-    vec[r]->val=temp;
-    l++;
-    r--;
+// if(level%2==1){ //odd level
+// int l=0,r=vec.size()-1;
+// while(l<r){
+//     int temp=vec[l]->val;
+//     vec[l]->val=vec[r]->val;
+//     vec[r]->val=temp;
+//     l++;
+//     r--;
+// }
+// }
+//             level++;
+//         }
+//         return root;
+//     }
+    TreeNode* reverseOddLevels(TreeNode* root) {
+queue<TreeNode*>q;
+int level=0;
+q.push(root);
+while(!q.empty()){
+    int size=q.size();
+    vector<TreeNode*>vec;
+    while(size--){
+        TreeNode* node= q.front();
+        q.pop();
+        vec.push_back(node);
+        if(node->left) q.push(node->left);
+        if(node->right) q.push(node->right);
+    
+    }
+    int l=0, r=vec.size()-1;
+    if(level%2==1){
+    while(l<r){
+        int temp=vec[l]->val;
+        vec[l]->val=vec[r]->val;
+        vec[r]->val=temp;
+        l++;
+        r--;
+    }
+    }
+    level++;
+
 }
-}
-            level++;
-        }
-        return root;
+return root;
     }
 };
