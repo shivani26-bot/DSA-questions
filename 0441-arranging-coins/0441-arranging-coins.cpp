a511e1b897a9d18c0de3520ab88cccdd47104b24
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-04-09 09:56:23 +0530

    Time: 0 ms (100%), Space: 8.7 MB (68.02%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-      int step=1;//step will contain that amount of coins, if step=1, 1 coin, step=2, 2 coin and so on...
-      int result=0;
-      while(n>=step){
-        n-=step;
-        result++;
-        step++;
-      }
-      return result;
-
+        // approach1: start from step =1, and for every step check n >= step, for that steps decrement the coin coint as well 
+    //   int step=1;//step will contain that amount of coins, if step=1, 1 coin, step=2, 2 coin and so on...
+     
+    //   int result=0;
+    //   while(n>=step){
+    //     n-=step;
+    //     result++;
+    //     step++;
+    //   }
+    //   return result;
+// approach2: binary search 
+// for every mid, find mid*(mid+1)/2<=n
+long l=0,h=n;
+while(l<=h){
+   long mid=l+(h-l)/2;
+    if(n>=(mid*(mid+1)/2)) l=mid+1;
+    else h=mid-1;
+}
+return (int)l-1;
     }
 };
