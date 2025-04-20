51738736860251d765b53ea284c9343aaecaac90
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-04-20 19:37:07 +0530

    Time: 10 ms (73.94%), Space: 35.1 MB (9.15%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -1,40 +1,69 @@
 class Solution {
 public:
- bool func(vector<int>& A, int N,int days, int mid){
-        int sum=0, count=1;
-        for(int i=0;i<N;i++){
-            if(sum+A[i]>mid) {
-                count++;
-                sum=A[i];
-            }
-            else{
-                sum+=A[i];
-            }
-        }
-        return count<=days;
-    }
-    int shipWithinDays(vector<int>& A, int days) {
-        int N= A.size();
-         int low= *max_element(A.begin(),A.end());
-        int sum=0;
-        for(int i=0;i<N;i++){
-          sum+=A[i];
+//  bool func(vector<int>& A, int N,int days, int mid){
+//         int sum=0, count=1;
+//         for(int i=0;i<N;i++){
+//             if(sum+A[i]>mid) {
+//                 count++;
+//                 sum=A[i];
+//             }
+//             else{
+//                 sum+=A[i];
+//             }
+//         }
+//         return count<=days;
+//     }
+//     int shipWithinDays(vector<int>& A, int days) {
+//         int N= A.size();
+//          int low= *max_element(A.begin(),A.end());
+//         int sum=0;
+//         for(int i=0;i<N;i++){
+//           sum+=A[i];
             
-        }
+//         }
        
-        int high=sum;
-        //  while(low<high){
-        while(low<high){
-            int mid=(low+high)/2;
-            bool r = func(A,N,days,mid);
-            if(!r) low=mid+1;
-            else{
-                // bool r1= func(A,N,days,mid-1);
-                // if(!r1) return mid;
-                // else high=mid-1;
-                high=mid;
-            }
-        }
-   return low;
+//         int high=sum;
+//         while(low<=high){
+//             int mid=(low+high)/2;
+//             bool r = func(A,N,days,mid);
+//             if(!r) low=mid+1;
+//             else{
+//                 bool r1= func(A,N,days,mid-1);
+//                 if(!r1) return mid;
+//                 else high=mid-1;
+//             }
+//         }
+//    return low;
+//     }
+
+bool canShipAllPackages(vector<int>& weights, int days, int w){
+int n= weights.size();
+int sum=0; //sum of weight of packages
+int actualDays=1;
+for(int i=0;i<n;i++){
+    if(sum+weights[i]>w){
+        actualDays++;
+        sum=weights[i];
+    }
+    else
+    sum+=weights[i];
+}
+return actualDays<=days;
+}
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n=weights.size();
+int total_weight=0;
+for(int i=0;i<n;i++){
+    total_weight+=weights[i];
+}
+int low=  *max_element(weights.begin(),weights.end());
+int high= total_weight;
+while(low<high){
+    int mid=low+(high-low)/2; //possible weight
+    if(canShipAllPackages(weights, days, mid)) high=mid;
+    else low=mid+1;
+}
+return low;
+
     }
 };
