ccee679f02f64d6b2c7b8bf60e90ec3111b7a7a9
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-04-21 22:17:12 +0530

    Time: 0 ms (100%), Space: 17.5 MB (52.38%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -49,8 +49,48 @@ public:
 //      return ans;
 //     }
 
+// int leftMost(vector<int>& arr, int target,int n){
+// int left_most_index=-1;
+//     int low=0;
+//     int high=n-1;
+//     while(low<=high){
+//         int mid=low+(high-low)/2;
+//         if(arr[mid]<target){
+//             low=mid+1;
+//         }
+//         else if(arr[mid]>target){
+//             high=mid-1;
+//         }
+//         else{
+//             left_most_index=mid;
+//             high=mid-1;
+//         }
+//     }
+//     return left_most_index;
+// }
+// int rightMost(vector<int>& arr, int target,int n){
+// int right_most_index=-1;
+//     int low=0;
+//     int high=n-1;
+//     while(low<=high){
+//         int mid=low+(high-low)/2;
+//         if(arr[mid]<target){
+//             low=mid+1;
+//         }
+//         else if(arr[mid]>target){
+//             high=mid-1;
+//         }
+//         else{
+//             right_most_index=mid;
+//             low=mid+1;
+//         }
+//     }
+//     return right_most_index;
+// }
+
+
 int leftMost(vector<int>& arr, int target,int n){
-int left_most_index=-1;
+
     int low=0;
     int high=n-1;
     while(low<=high){
@@ -62,14 +102,16 @@ int left_most_index=-1;
             high=mid-1;
         }
         else{
-            left_most_index=mid;
+            if(mid==0) return mid;
+            else if(arr[mid-1]!=target) return mid;
+            else 
             high=mid-1;
         }
     }
-    return left_most_index;
+    return -1;
 }
 int rightMost(vector<int>& arr, int target,int n){
-int right_most_index=-1;
+
     int low=0;
     int high=n-1;
     while(low<=high){
@@ -81,11 +123,12 @@ int right_most_index=-1;
             high=mid-1;
         }
         else{
-            right_most_index=mid;
-            low=mid+1;
+           if(mid==n-1) return mid;
+           else if(arr[mid+1]!=target) return mid;
+           else low=mid+1;
         }
     }
-    return right_most_index;
+    return -1;
 }
   vector<int> searchRange(vector<int>& arr, int target) {
      int n=arr.size();
