36f6a3f14884f51849ba3ea961e2cad0409ba96a
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-04-29 17:50:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+ int m;
+int binarySearch(vector<vector<int>>& items, int target){
+    int low=0;
+    int high=m-1;
+    int ans=0;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(items[mid][0]>target) high=mid-1;
+        else{
+            ans=max(ans,items[mid][1]);
+            low=mid+1;
+        }
+    }
+    return ans;
+}
+    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
+        vector<int>ans;
+        int n=queries.size();
+     m=items.size();
+        sort(items.begin(),items.end());
+        for(auto it:items){
+            cout<<it[0]<<" "<<it[1]<<endl;
+        }
+        for(int i=0;i<n;i++){
+                  int q=queries[i];
+                 ans.push_back(binarySearch(items,q));
+        }
+        return ans;
+    }
+};
