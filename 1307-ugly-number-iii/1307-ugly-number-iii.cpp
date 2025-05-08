0c94b09d3f958c7120dfbcbb99d59ca973e57d53
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-05-08 23:19:42 +0530

    Time: 0 ms (100%), Space: 7.8 MB (79.93%) - LeetHub

--- 1307-ugly-number-iii/1307-ugly-number-iii.cpp
+++ 1307-ugly-number-iii/1307-ugly-number-iii.cpp
@@ -51,3 +51,36 @@ ll lcm(ll a, ll b) {
 // ll lcm(ll a, ll b) {
 //     return a / __gcd(a, b) * b; 
 // }
+
+
+// n=100, a=2, b=3, c=5
+// All numbers divisible by 2 from 1 to 100:
+// 2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100
+// (50 numbers in total)
+
+// All numbers divisible by 3 from 1 to 100:
+// 3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60,63,66,69,72,75,78,81,84,87,90,93,96,99
+// (33 numbers in total)
+
+// All numbers divisible by 5 from 1 to 100:
+// 5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100
+// (20 numbers in total)
+
+// Divisible by both 2 and 3 (LCM = 6):
+// All numbers divisible by 6 from 1 to 100:
+// 6,12,18,24,30,36,42,48,54,60,66,72,78,84,90,96
+// (16 numbers in total)
+
+// Divisible by both 3 and 5 (LCM = 15):
+// All numbers divisible by 15 from 1 to 100:
+// 15,30,45,60,75,90
+// (6 numbers in total)
+
+// Divisible by both 2 and 5 (LCM = 10):
+// All numbers divisible by 10 from 1 to 100:
+// 10,20,30,40,50,60,70,80,90,100
+// (10 numbers in total)
+
+// Divisible by All Three Numbers (2, 3, and 5) (LCM = 30)
+// All numbers divisible by 30 from 1 to 100:
+// (3 numbers in total)
