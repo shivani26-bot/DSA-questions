43e4fee9171dffa44c9653ac39985d1a07809e5f
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-05-12 08:46:26 +0530

    Time: 5 ms (9.66%), Space: 9.7 MB (94.39%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-       string newString="";
-        int n=s.length();
-        for(int i=0;i<n;i++){
-            if(s[i]>='A' && s[i]<='Z'){
-                s[i]=s[i]+32;
-                cout<<s[i]<<endl;
-            }
-            if((s[i]>='a' && s[i]<='z')||( s[i]>='0' && s[i]<='9')){
+//        string newString="";
+//         int n=s.length();
+//         for(int i=0;i<n;i++){
+//             if(s[i]>='A' && s[i]<='Z'){
+//                 s[i]=s[i]+32;
+//                 cout<<s[i]<<endl;
+//             }
+//             if((s[i]>='a' && s[i]<='z')||( s[i]>='0' && s[i]<='9')){
        
-                newString+=s[i];
-            }
-        }
-cout<<newString;
-        int m=newString.length();
-        int i=0, j=m-1;
-        while(i<=j){
-            if(newString[i]!=newString[j]) return false;
-            i++;
-            j--;
-        }
-        return true;
+//                 newString+=s[i];
+//             }
+//         }
+// cout<<newString;
+//         int m=newString.length();
+//         int i=0, j=m-1;
+//         while(i<=j){
+//             if(newString[i]!=newString[j]) return false;
+//             i++;
+//             j--;
+//         }
+//         return true;
+
+int n=s.length();
+int i=0,j=n-1;
+while(i<=j){
+    if(!isalnum(s[i])){i++;continue;}
+    if(!isalnum(s[j])){j--;continue;}
+    if(tolower(s[i])!=tolower(s[j])) return false;
+    else{
+        i++;
+        j--;
+    }
+}
+return true;
     }
 };
