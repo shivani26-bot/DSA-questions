43624774082b57db5462d90181a9b8c2fabd9317
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-05-13 15:08:35 +0530

    Time: 0 ms (100%), Space: 8.4 MB (21.87%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
 
-int fib(int n, vector<int>&dp){
-if(n<=1) return n;
-      if(n==2) return 1;
-      if(dp[n]!=-1) return dp[n];
+// int fib(int n, vector<int>&dp){
+// if(n<=1) return n;
+//       if(n==2) return 1;
+//       if(dp[n]!=-1) return dp[n];
 
-      dp[n]= fib(n-1,dp)+fib(n-2,dp)+fib(n-3,dp);
-      return dp[n];
-}
+//       dp[n]= fib(n-1,dp)+fib(n-2,dp)+fib(n-3,dp);
+//       return dp[n];
+// }
+//     int tribonacci(int n) {
+//         //recursion
+//     //   if(n<=1) return n;
+//     //   if(n==2) return 1;
+
+//     //   return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+
+//       //dp
+//      vector<int>dp(n+1,-1);
+
+//     return fib(n, dp);
+      
+
+//     }
+
+//tabulation
     int tribonacci(int n) {
-        //recursion
-    //   if(n<=1) return n;
-    //   if(n==2) return 1;
-
-    //   return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
-
-      //dp
+if(n<=1) return n;
+if(n==2) return 1;
      vector<int>dp(n+1,-1);
-
-    return fib(n, dp);
+dp[0]=0,dp[1]=1,dp[2]=1;
+for(int i=3;i<=n;i++){
+dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+}
+    return dp[n];
       
 
     }
