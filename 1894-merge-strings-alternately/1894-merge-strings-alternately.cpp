e4e3b428d974c168cfb62ddc402ceac447121042
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-05-13 11:01:13 +0530

    Time: 0 ms (100%), Space: 8.5 MB (62.43%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
-    string mergeAlternately(string word1, string word2) {
-        int n=word1.size(),m=word2.size();
-        bool flag=false;
-        int i=0,j=0;
-        string newStr="";
-        while(i<n || j<m){
-            if(!flag){
-                 flag=true;
-                if(i>=n) continue;
-                newStr+=word1[i];
-                i++;
+    // string mergeAlternately(string word1, string word2) {
+    //     int n=word1.size(),m=word2.size();
+    //     bool flag=false;
+    //     int i=0,j=0;
+    //     string newStr="";
+    //     while(i<n || j<m){
+    //         if(!flag){
+    //              flag=true;
+    //             if(i>=n) continue;
+    //             newStr+=word1[i];
+    //             i++;
                
-            }
-            else{
-                   flag=false;
-                        if(j>=m) continue;
-                newStr+=word2[j];
-                j++;
+    //         }
+    //         else{
+    //                flag=false;
+    //                     if(j>=m) continue;
+    //             newStr+=word2[j];
+    //             j++;
              
-            }
-        }
-        return newStr;
+    //         }
+    //     }
+    //     return newStr;
+    // }
+
+        string mergeAlternately(string word1, string word2) {
+        int n=word1.size(),m=word2.size();
+int k=min(n,m);//shortest length;
+int i=0;
+string newStr="";
+while(i<k){
+    newStr+=word1[i];
+    newStr+=word2[i];
+    i++;
+}
+if(n>m){
+    newStr+= word1.substr(k);
+}
+if(m>n){
+    newStr+= word2.substr(k);
+}
+return newStr;
     }
 };
