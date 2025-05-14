709e1c04490c2d5237fc66d9d699aab0a9b99f10
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-05-14 12:05:32 +0530

    Time: 1185 ms (5.67%), Space: 9.3 MB (93.29%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -21,14 +21,14 @@ public:
 // (1,4)->(2,3) repeating subproblems
 // we can store in dp array for (0,5) that (0,5) is true, (1,4) is true, (2,3) is true
 // when we call (1,4) no need to check further and we can directly return true 
-// bool checkPalindrome(int i, int j, string &s){
-//     while(i<=j){
-//         if(s[i]!=s[j]) return false;
-//         i++;
-//         j--;
-//     }
-//     return true;
-// }
+bool checkPalindrome(int i, int j, string &s){
+    while(i<=j){
+        if(s[i]!=s[j]) return false;
+        i++;
+        j--;
+    }
+    return true;
+}
 //recursive
 // o(n^3)
 // bool checkPalindrome(int i, int j, string &s){
@@ -36,42 +36,14 @@ public:
 //         if(s[i]==s[j]) return checkPalindrome(i+1,j-1,s);
 // return false;
 // }
-//     string longestPalindrome(string s) {
-//         int n=s.length();
-        
-// int maxLen=INT_MIN;
-// int sp=0; //starting point of the substring
-//         for(int i=0;i<n;i++){//o(n^2)
-//             for(int j=i;j<n;j++){
-//                 if(checkPalindrome(i,j,s)==true){ //o(n)
-//                    if(j-i+1>maxLen){
-//                     maxLen=j-i+1;
-//                     sp=i;
-//                    }
-//                 }
-//             }
-//         }
-//         return s.substr(sp,maxLen);
-
-//     }
-
-//dp
-//o(n^3)
-int dp[1001][1001];
-bool checkPalindrome(int i, int j, string &s){
-   if(i>=j) return 1; //true
-   if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==s[j]) return dp[i][j]=checkPalindrome(i+1,j-1,s);
-return dp[i][j]=0; //false
-}
     string longestPalindrome(string s) {
         int n=s.length();
-       memset(dp,-1, sizeof(dp)); 
+        
 int maxLen=INT_MIN;
 int sp=0; //starting point of the substring
-        for(int i=0;i<n;i++){ //o(n^2)
+        for(int i=0;i<n;i++){//o(n^2)
             for(int j=i;j<n;j++){
-                if(checkPalindrome(i,j,s)){
+                if(checkPalindrome(i,j,s)==true){ //o(n)
                    if(j-i+1>maxLen){
                     maxLen=j-i+1;
                     sp=i;
@@ -80,7 +52,35 @@ int sp=0; //starting point of the substring
             }
         }
         return s.substr(sp,maxLen);
+
     }
+
+//dp
+//o(n^3)
+// int dp[1001][1001];
+// bool checkPalindrome(int i, int j, string &s){
+//    if(i>=j) return 1; //true
+//    if(dp[i][j]!=-1) return dp[i][j];
+//         if(s[i]==s[j]) return dp[i][j]=checkPalindrome(i+1,j-1,s);
+// return dp[i][j]=0; //false
+// }
+//     string longestPalindrome(string s) {
+//         int n=s.length();
+//        memset(dp,-1, sizeof(dp)); 
+// int maxLen=INT_MIN;
+// int sp=0; //starting point of the substring
+//         for(int i=0;i<n;i++){ //o(n^2)
+//             for(int j=i;j<n;j++){
+//                 if(checkPalindrome(i,j,s)){
+//                    if(j-i+1>maxLen){
+//                     maxLen=j-i+1;
+//                     sp=i;
+//                    }
+//                 }
+//             }
+//         }
+//         return s.substr(sp,maxLen);
+//     }
 
 
 };
