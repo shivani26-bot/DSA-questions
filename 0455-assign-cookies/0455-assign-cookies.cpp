f68d54b971271d7a838e817ebd60992a640191c9
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-05-15 12:41:45 +0530

    Time: 14 ms (21.11%), Space: 44.9 MB (43.3%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -6,17 +6,31 @@ public:
 sort(g.begin(),g.end());
 sort(s.begin(),s.end());
 int i=0,j=0;
-int count=0;
+
 while(i<n && j<m){
     if(s[j]>=g[i]){
-        count++;
-        j++;
-            i++;
-    }
-   else if(g[i]>s[j]) j++;
 
+        i++;
+    }
+    j++;
 }
-return count;
+return i;
+
+
+
+
+
+// int count=0;
+// while(i<n && j<m){
+//     if(s[j]>=g[i]){
+//         count++;
+//         j++;
+//             i++;
+//     }
+//    else if(g[i]>s[j]) j++;
+
+// }
+// return count;
 
     }
 };
