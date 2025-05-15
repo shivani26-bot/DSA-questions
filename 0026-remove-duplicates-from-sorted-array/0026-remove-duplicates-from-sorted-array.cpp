fd26ecbc862035fe7c4332999d4fa7bb03ed22c0
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-05-15 11:46:38 +0530

    Time: 0 ms (100%), Space: 22.4 MB (96.57%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -10,7 +10,8 @@ int i=1;
 for(int j=1;j<n;j++){
     if(nums[j]!=nums[i-1]){
         //unique element is found
-        nums[i]=nums[j];
+        // nums[i]=nums[j];
+        swap(nums[i],nums[j]);
         i++;
     }
 }
