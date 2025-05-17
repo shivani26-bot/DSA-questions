241b995d5dc5212fe3eb1f60ff225f13e1f5cf79
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-05-17 15:59:21 +0530

    Time: 15 ms (3.34%), Space: 13 MB (87.97%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -1,26 +1,48 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
-        int n=nums.size();
-        int i=0;
+        // int n=nums.size();
+        // int i=0;
+        // while(i<n){
+        //    if(i<n-1 && nums[i]==nums[i+1]){
+        //     nums[i]*=2;
+        //     nums[i+1]=0;
+        //     i++;
+        //    }
+        //   i++;
+        // }
+        // for(int i=0;i<n;i++)
+        // cout<<nums[i];
+        // int j=0,k=0;
+        // while(k<n){
+        //     if(nums[k]!=0){
+        //      swap(nums[j],nums[k]);
+        //      j++;
+        //     }
+        //     k++;
+        // }
+        // return nums;
+
+
+        // one pass 
+              int n=nums.size();
+        int i=0,j=0;
         while(i<n){
-           if(i<n-1 && nums[i]==nums[i+1]){
+           if(i<n-1 && nums[i]==nums[i+1] && nums[i]!=0){
             nums[i]*=2;
             nums[i+1]=0;
-            i++;
+           }
+           if(nums[i]!=0){
+            if(i!=j){
+                swap(nums[i],nums[j]);
+            }
+            j++;
            }
           i++;
         }
         for(int i=0;i<n;i++)
         cout<<nums[i];
-        int j=0,k=0;
-        while(k<n){
-            if(nums[k]!=0){
-             swap(nums[j],nums[k]);
-             j++;
-            }
-            k++;
-        }
+      
         return nums;
     }
 };
