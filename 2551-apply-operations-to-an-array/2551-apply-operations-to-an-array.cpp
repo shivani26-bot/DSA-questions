eb80f7058f205a86472189d8f3486d3d71939401
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-05-17 16:03:44 +0530

    Time: 15 ms (3.34%), Space: 13.3 MB (24.98%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -27,6 +27,7 @@ public:
         // one pass 
               int n=nums.size();
         int i=0,j=0;
+        //j maintains the pointer to 0,and i to non negative number
         while(i<n){
            if(i<n-1 && nums[i]==nums[i+1] && nums[i]!=0){
             nums[i]*=2;
