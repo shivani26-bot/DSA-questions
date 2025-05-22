65db9ed2a90826c1e75ff05702a47c62cca57640
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-05-22 12:20:41 +0530

    Time: 43 ms (86.73%), Space: 33.1 MB (23.35%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -52,7 +52,7 @@ void twoSum(vector<int>& nums,int target,int s,int e){
 
         int n=nums.size();
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-2;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
             
             int n1=nums[i];
