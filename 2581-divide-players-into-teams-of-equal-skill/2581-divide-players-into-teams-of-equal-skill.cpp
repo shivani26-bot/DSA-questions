d9ae535fb2d62abc1c459a49ac7eb412b935f670
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-05-25 15:13:23 +0530

    Time: 11 ms (84.22%), Space: 56.5 MB (58.37%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
@@ -22,3 +22,42 @@ while(i<=j){
         return chemistry;
     }
 };
+
+
+// class Solution {
+// public:
+// // time complexity:o(n)
+//     long long dividePlayers(vector<int>& skill) {
+//         int n=skill.size();
+//          long long int sum=0;
+//         for(int i=0;i<n;i++){  //o(n)
+//             sum+=skill[i];
+//         }
+//         int total_pairs=n/2;
+//         if(total_pairs==1) return skill[0]*skill[1];
+//         if(sum%total_pairs!=0) 
+//         {
+//             return -1;
+//         }
+//         int target= sum/total_pairs;
+//         unordered_map<int,int>mp;
+//         int countPairs=0;
+// cout<<target<<endl;
+//         long long int chemistry=0;
+//         for(int i=0;i<n;i++){ //o(n)
+//                int num=target-skill[i];
+//                if(mp.find(num)!=mp.end()){ //o(1)
+//                    chemistry+= (skill[i]*num);
+//                     mp[num]--;
+//                     countPairs++;
+//                     if(mp[num]==0) mp.erase(num);
+//                }
+//                else
+//                mp[skill[i]]++;
+//         }
+
+// if(countPairs!=total_pairs || chemistry==0 ) return -1;
+
+// return chemistry;
+//     }
+// };
