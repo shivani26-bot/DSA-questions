0a932bcb07ad03070a0c83ad82382cfe709eed6a
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-05-26 23:07:05 +0530

    Time: 0 ms (100%), Space: 99.9 MB (64.44%) - LeetHub

--- 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
         int n=neededTime.size();
+        //i will be pointing to previous max element and j will be pointing to next element
         int minTime=0;
        int i=0, j=1;
        while( j<n){
