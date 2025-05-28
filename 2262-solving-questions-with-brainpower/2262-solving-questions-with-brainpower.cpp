7ba209442ec7d4e8e221aa2a48e7afba3601c343
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-05-28 14:56:00 +0530

    Time: 16 ms (9.9%), Space: 129.2 MB (6.09%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+// int n;
+// long long solve(int index,vector<vector<int>>& questions, int n){
+// if(index>=n) return 0;
+
+// int points= questions[index][0];
+// int brainPower= questions[index][1];
+//     int notTake= solve(index+1, questions, n);
+//     int take= points+solve(index+brainPower+1, questions,n);
+//     return max(take, notTake);
+// }
+//     long long mostPoints(vector<vector<int>>& questions) {
+//         n=questions.size();
+//         return solve(0, questions,n);
+//     }
+
+
+
+
+int n;
+long long solve(int index,vector<vector<int>>& questions, int n, vector<long long>&dp){
+if(index>=n) return 0;
+
+
+long long points= questions[index][0];
+long long brainPower= questions[index][1];
+if(dp[index]!=-1) return dp[index];
+    long long notTake= solve(index+1, questions, n,dp);
+    long long take= points+solve(index+brainPower+1, questions,n,dp);
+    return dp[index]=max(take, notTake);
+}
+    long long mostPoints(vector<vector<int>>& questions) {
+        n=questions.size();
+        vector<long long>dp(n,-1);
+        return solve(0, questions,n,dp);
+    }
+};
