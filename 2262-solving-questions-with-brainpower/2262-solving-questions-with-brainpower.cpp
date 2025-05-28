1d43858756fe24328d3e85ce45000e0df3b4656c
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-05-28 15:04:14 +0530

    Time: 0 ms (100%), Space: 119.8 MB (54.14%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -18,21 +18,42 @@ public:
 
 
 
+// int n;
+// long long solve(int index,vector<vector<int>>& questions, int n, vector<long long>&dp){
+// if(index>=n) return 0;
+
+
+// long long points= questions[index][0];
+// long long brainPower= questions[index][1];
+// if(dp[index]!=-1) return dp[index];
+//     long long notTake= solve(index+1, questions, n,dp);
+//     long long take= points+solve(index+brainPower+1, questions,n,dp);
+//     return dp[index]=max(take, notTake);
+// }
+//     long long mostPoints(vector<vector<int>>& questions) {
+//         n=questions.size();
+//         vector<long long>dp(n,-1);
+//         return solve(0, questions,n,dp);
+//     }
+
+
 int n;
-long long solve(int index,vector<vector<int>>& questions, int n, vector<long long>&dp){
-if(index>=n) return 0;
-
-
-long long points= questions[index][0];
-long long brainPower= questions[index][1];
-if(dp[index]!=-1) return dp[index];
-    long long notTake= solve(index+1, questions, n,dp);
-    long long take= points+solve(index+brainPower+1, questions,n,dp);
-    return dp[index]=max(take, notTake);
-}
     long long mostPoints(vector<vector<int>>& questions) {
         n=questions.size();
-        vector<long long>dp(n,-1);
-        return solve(0, questions,n,dp);
+        vector<long long>dp(n+1,-1);
+        dp[n]=0;
+        for(int index=n-1;index>=0;index--){
+long long points= questions[index][0];
+long long brainPower= questions[index][1];
+  long long notTake= dp[index+1];
+    long long take= points;
+    
+    if(index+brainPower+1<n){
+       take= points+dp[index+brainPower+1];
+    }
+
+    dp[index]=max(take, notTake);
+        }
+        return dp[0];
     }
 };
