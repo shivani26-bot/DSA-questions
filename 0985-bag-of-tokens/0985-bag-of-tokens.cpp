b87e296a86bde65a3dad7a08296c4cf75f114e8f
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-05-29 14:39:07 +0530

    Time: 6 ms (4.41%), Space: 13.1 MB (100%) - LeetHub

--- 0985-bag-of-tokens/0985-bag-of-tokens.cpp
+++ 0985-bag-of-tokens/0985-bag-of-tokens.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
-        int n=tokens.size();
-        sort(tokens.begin(),tokens.end());
-          int i=0,j=n-1;
-   int score=0,maxScore=0;
-if(n==1){
-    if(power>=tokens[0]) return score+=1;
-    else return 0;
-}
- 
-   while(i<=j){
-if(power>=tokens[i]){
-    score+=1;
-  
-    power-=tokens[i];
-      cout<<power<<" "<<score<<endl;
-    i++;
-}
-else if(score>=1){
-    power+=tokens[j];
-    score-=1;
-  
-     cout<<power<<" "<<score<<endl;
-    j--;
-}
-else {
-    i++;
-    
-}
-  maxScore=max(score,maxScore);
-   }
+        
+         sort(tokens.begin(),tokens.end());
+          int i=0,j=tokens.size()-1;
+          int score=0;
+          int maxscore=0;
+         while(i<=j){
 
- 
-   return maxScore;
+             
+            if(power>=tokens[i]){
+                 power-=tokens[i];
+                 score++;
+                 maxscore=max(maxscore,score);
+                 i++;
+             }
+             else if(score>=1){
+                 power+=tokens[j];
+                 score--;
+                 j--;
+             }
+         else break;
+             
+         }
+         return maxscore;
     }
 };
