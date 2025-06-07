9ade318d56a07e0abd996c53379fba57dbc3e808
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-06-07 13:02:20 +0530

    Time: 0 ms (100%), Space: 8.7 MB (83.75%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -4,13 +4,9 @@ public:
         stack<char>st;
         for(auto ch:s){
             if(!st.empty() && ch==')' && st.top()=='(') st.pop();
-                else if(!st.empty() && ch==']' && st.top()=='[') st.pop();
-                else if(!st.empty() && ch=='}' && st.top()=='{') st.pop();
-            else{
-
-                st.push(ch);
-               
-            }
+            else if(!st.empty() && ch==']' && st.top()=='[') st.pop();
+            else if(!st.empty() && ch=='}' && st.top()=='{') st.pop();
+            else st.push(ch);
         }
         return st.empty();
     }
