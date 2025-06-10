2be1203e4dfffd438811e478cacee34a0a1f6345
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-06-10 08:54:21 +0530

    Time: 0 ms (100%), Space: 17.2 MB (16.85%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,3 +1,55 @@
+
+// class Solution {
+// public:
+// for each token, we either perform a constant time operation 
+// (such as checking if it is a number) or recursively call the evalRPN function on a smaller input vector
+// time :o(n)
+// space: o(n)
+    // int evalRPN(vector<string>& tokens) {
+    //     string token=tokens.back();
+    //     tokens.pop_back();
+    //     if(token!="*" && token!="/" && token!="+" && token!="-" )
+    //     return stoi(token);
+    //     else
+    //     {
+    //         long long a= evalRPN(tokens);
+    //         long long b= evalRPN(tokens);
+    //         if(token=="+") return b+a;
+    //         else if(token=="-") return b-a;
+    //         else if(token=="/") return b/a;
+    //         else return b*a;
+    //     }
+    // }
+// bool isOperator(string &token){
+//     if(token=="+"||token=="-"||token=="/"||token=="*") return true;
+//     else return false;
+// }
+// int evaluate(int a,int b, string &token){
+//     if(token=="+") return b+a;
+//     if(token=="-") return b-a;
+//     if(token=="*") return b*a;
+//     if(token=="/") return b/a;
+//     return 0;
+    
+// }
+//     int evalRPN(vector<string>& tokens) {
+//       stack<int>st;
+//       for(auto token: tokens)
+//       {
+//           if(isOperator(token)){
+//                int a=st.top();
+//                st.pop();
+//                int b=st.top();
+//                st.pop();
+//                st.push(evaluate(a,b,token));
+//           }
+//           else{
+//               st.push(stoi(token));
+//           }
+//       }
+//       return st.top();
+//     }
+// };
 class Solution {
 public:
 
