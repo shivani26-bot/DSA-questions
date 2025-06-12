1af379d9ed12d2b6a8136c610efcbb92e5bbe48f
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-06-12 18:46:10 +0530

    Time: 0 ms (100%), Space: 22.2 MB (41.57%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int>st;
+        
+        int n=asteroids.size();
+        for(int i=0;i<n;i++){
+bool alive=true;
+         while(!st.empty() && st.top() >0 && asteroids[i]<0) {
+            if(st.top()<-asteroids[i]) {//only one asteroid will be destroyed
+                st.pop();
+                continue;
+            }
+            else if(st.top()==-asteroids[i]){ //both the asteroids will be destroyed and alive will be false;
+                st.pop();
+            }
+            alive=false;
+            break;
+         }
+   if(alive)
+            st.push(asteroids[i]);
+
+        }
+vector<int>ans;
+while(!st.empty()){
+    ans.push_back(st.top());
+    st.pop();
+}
+reverse(ans.begin(),ans.end());
+return ans;
+    }
+};
+
+// [-2,-1,1,2]
+// output:[-2,-1,1,2]
