9371b879150cc85e543409594b90eef90b6c7003
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-06-13 15:00:19 +0530

    Time: 0 ms (100%), Space: 11.6 MB (85.01%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -22,29 +22,58 @@ int solve(int i, int j, vector< vector< int> > &obstacleGrid,vector<vector<int>>
 //    return solve(n-1,m-1,obstacleGrid,dp);
 //     }
 
+//     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+//         	int m=obstacleGrid.size(); //row size
+//             int n=obstacleGrid[0].size();//column size
+//             vector<vector<int>>dp(m,vector<int>(n,0));
+//    for(int i=0;i<m;i++){
+//     for(int j=0;j<n;j++){
+      
+
+//         if(obstacleGrid[i][j]!=1){
+//               if(i==0 && j==0){
+//             dp[i][j]=1;
+//             continue;
+//         }
+//         int left=0 ,up=0;
+// if(j>0)
+//         left =dp[i][j-1];
+// if(i>0)
+//         up= dp[i-1][j];
+
+// dp[i][j]=left+up;
+//     }
+//     }
+//    }
+//    return dp[m-1][n-1];
+//     }
+
+
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         	int m=obstacleGrid.size(); //row size
             int n=obstacleGrid[0].size();//column size
-            vector<vector<int>>dp(m,vector<int>(n,0));
+            vector<int>prev(n,0);
    for(int i=0;i<m;i++){
+    vector<int>curr(n,0);
     for(int j=0;j<n;j++){
       
 
         if(obstacleGrid[i][j]!=1){
               if(i==0 && j==0){
-            dp[i][j]=1;
+           curr[j]=1;
             continue;
         }
         int left=0 ,up=0;
 if(j>0)
-        left =dp[i][j-1];
+        left =curr[j-1];
 if(i>0)
-        up= dp[i-1][j];
+        up= prev[j];
 
-dp[i][j]=left+up;
+curr[j]=left+up;
     }
     }
+    prev=curr;
    }
-   return dp[m-1][n-1];
+   return prev[n-1];
     }
 };
