5ce75a280db14d080a1e26be9af714446935408a
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-06-24 22:16:10 +0530

    Time: 0 ms (100%), Space: 8.6 MB (29.32%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -12,3 +12,21 @@ public:
         return st.size();
     }
 };
+
+
+// class Solution {
+// public:
+//     //o(n)
+//     int minAddToMakeValid(string s) {
+//         int n=s.length();
+//         stack<char>st;
+//         for(int i=0;i<n;i++){
+//         if(!st.empty() && s[i]==')' && st.top()=='(') {
+//                st.pop();
+//                 continue;
+//            }
+//          st.push(s[i]);
+//         }
+//         return st.size();
+//     }
+// };
