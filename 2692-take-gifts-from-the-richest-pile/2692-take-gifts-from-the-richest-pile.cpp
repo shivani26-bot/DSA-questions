a5c47ab3c1c8956dbee3c99bf005da9dbf36d6aa
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-02 11:33:52 +0530

    Time: 0 ms (100%), Space: 13.5 MB (6.61%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
+// n+klogk+nlogn
     long long pickGifts(vector<int>& gifts, int k) {
-//         priority_queue<int>pq(gifts.begin(),gifts.end());
-
-//         while(k>0){
-//             int x=pq.top();
-//             pq.pop();
-//             int y= floor(sqrt(x));
-//             pq.push(y);
-//             k--;
-//         }
-// long long sum=0;
-//         while(!pq.empty()){
-//             int x=pq.top();
-//             pq.pop();
-//             sum+=x;
-
-//         }
-//         return sum;
-//     }
+        //n heapify
 priority_queue<int>pq(gifts.begin(),gifts.end());
 
+// klogn
 while(k-->0){
-    int x=pq.top();
-    pq.pop();
+    int x=pq.top(); //o(1)
+    pq.pop();//log n
     int y= floor(sqrt(x));
-    pq.push(y);
+    pq.push(y);//logn
 }
 long long sum=0;
+//nlogn  // atmost n elements can be there in pq
 while(!pq.empty()){
       int x=pq.top();
       pq.pop();
