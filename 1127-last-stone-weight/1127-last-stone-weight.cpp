16d96a91614f74d2a312ed9a00faeef1a5811780
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-02 11:36:23 +0530

    Time: 0 ms (100%), Space: 10 MB (9.3%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.cpp
+++ 1127-last-stone-weight/1127-last-stone-weight.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
+//
     int lastStoneWeight(vector<int>& stones) {
         priority_queue<int>pq;//maxheap
-        //  priority_queue<int> pq(stones.begin(),stones.end());
+        //  priority_queue<int> pq(stones.begin(),stones.end());//o(n)
         int n=stones.size();
+        // nlogn
         for(int i=0;i<n;i++){
-            pq.push(stones[i]);
+            pq.push(stones[i]); //logn
         }
 
+// (n-1)logn
         while(pq.size()>1){
-            int p=pq.top();pq.pop();
-            int q=pq.top();pq.pop();
+            int p=pq.top();pq.pop();//logn
+            int q=pq.top();pq.pop();//logn
             if(p!=q){
                 int newStone= p-q;//new stone weight
-                pq.push(newStone);
+                pq.push(newStone);//logn
             }
         }
-        return pq.empty()?0:pq.top();
+        return pq.empty()?0:pq.top(); //o(1)
     }
 };
