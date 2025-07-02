e452535ce1e968409f2e72ae8aac5661424fa237
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-02 11:26:29 +0530

    Time: 0 ms (100%), Space: 27.7 MB (30.01%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
+// klogn+nlogn
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
 
 priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>>minHeap;
 int n=nums.size();
 for(int i=0;i<n;i++){
-    minHeap.push({nums[i],i});
+    minHeap.push({nums[i],i});//n logn
 }
+//k logn 
 while(k--){
-    int ele=minHeap.top().first;
-    int idx=minHeap.top().second;
-    minHeap.pop();
+    int ele=minHeap.top().first; //o(1)
+    int idx=minHeap.top().second; //o(1)
+    minHeap.pop(); // logn
     int newel= ele*multiplier;
     nums[idx]=newel;
     minHeap.push({newel,idx});
