c8f513e4ef00421ba468b385afd3668bc285d84b
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-03 10:50:43 +0530

    Time: 27 ms (87.48%), Space: 71.8 MB (58.07%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-struct comp {
-    bool operator() (vector<int>&p, vector<int>&q){
-        long d1=p[0]*p[0]+p[1]*p[1];
-        long d2=q[0]*q[0]+q[1]*q[1];
-        return d1>d2;
-    }
-};
+//   priority_queue<vector<int>,vetor<vector<int>>,comp>pq;
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<vector<int>>result;
-        priority_queue<vector<int>,vector<vector<int>>,comp>pq(points.begin(),points.end());
-        for(int i=0;i<k;i++){
-            vector<int>p=pq.top();
-            result.push_back(p);
+        priority_queue<pair<double,int>,vector<pair<double,int>>, greater<pair<double,int>>>pq;
+ vector<vector<int>> result;
+        int n= points.size();
+        for(int i=0;i<n;i++){
+            int x1=points[i][0];
+            int y1=points[i][1];
+            double dist= sqrt(pow(x1,2)+pow(y1,2));
+             pq.push({dist, i});
+        }
+
+        while(k--){
+            auto ele= pq.top();
+            int idx= ele.second;
+            result.push_back(points[idx]);
             pq.pop();
         }
         return result;
