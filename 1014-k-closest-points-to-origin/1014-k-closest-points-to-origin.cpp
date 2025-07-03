782f210ba3b876990886402936bb84ec1108b197
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-03 11:04:58 +0530

    Time: 73 ms (46.2%), Space: 77.9 MB (39.8%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
-//   priority_queue<vector<int>,vetor<vector<int>>,comp>pq;
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<double,int>,vector<pair<double,int>>, greater<pair<double,int>>>pq;
- vector<vector<int>> result;
-        int n= points.size();
-        for(int i=0;i<n;i++){
-            int x1=points[i][0];
-            int y1=points[i][1];
-            double dist= sqrt(pow(x1,2)+pow(y1,2));
-             pq.push({dist, i});
-        }
+struct comp {
+bool operator() (vector<int>&p, vector<int>&q){
+    return (p[0]*p[0]+p[1]*p[1])>(q[0]*q[0]+q[1]*q[1]);
+}
+};
 
-        while(k--){
-            auto ele= pq.top();
-            int idx= ele.second;
-            result.push_back(points[idx]);
-            pq.pop();
-        }
-        return result;
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+//         priority_queue<pair<double,int>,vector<pair<double,int>>, greater<pair<double,int>>>pq;
+//  vector<vector<int>> result;
+//         int n= points.size();
+//         // nlogn
+//         for(int i=0;i<n;i++){
+//             int x1=points[i][0];
+//             int y1=points[i][1];
+//             double dist= sqrt(pow(x1,2)+pow(y1,2));
+//              pq.push({dist, i});//logn
+//         }
+// // nlogk
+//         while(k--){
+//             auto ele= pq.top();
+//             int idx= ele.second;
+//             result.push_back(points[idx]);
+//             pq.pop();
+//         }
+//         return result;
+
+
+
+vector<vector<int>>result;
+priority_queue<vector<int>,vector<vector<int>>, comp>pq(points.begin(),points.end());
+while(k--){
+    vector<int>ele= pq.top();
+    result.push_back(ele);
+    pq.pop();
+}
+return result;
     }
 };
