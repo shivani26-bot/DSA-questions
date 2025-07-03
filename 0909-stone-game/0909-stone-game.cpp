1a1820cf31cf3bdb67c94ffbcc251471c0cc71ba
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-03 12:51:44 +0530

    Time: 0 ms (100%), Space: 10.2 MB (98.48%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -22,6 +22,11 @@ public:
 
 
 //top down
+//o(n^2)
+// i ∈ [0, n-1]
+// j ∈ [0, n-1], with i ≤ j
+// a ∈ {0, 1}
+// total valid states are n^2
 // int dp[500][500][2];
 // bool solve(int i, int j,int a,vector<int>&piles){
 // if(i>=j) return 0;
@@ -38,15 +43,17 @@ public:
 
 // return dp[i][j][a]=alice>bob;
 // }
-    bool stoneGame(vector<int>& piles) {
-    //     int n=piles.size();
-    //     memset(dp, -1,sizeof(dp));
-    //    return  solve(0,n-1,1,piles);
-
+//     bool stoneGame(vector<int>& piles) {
+//         int n=piles.size();
+//         memset(dp, -1,sizeof(dp));
+//        return  solve(0,n-1,1,piles);
     
+// }
 //maths
+ bool stoneGame(vector<int>& piles) {
+//  as alice is first one to make move alice will always choose 0, 2 4, 6 , n-2 even indexes, bob always choose 1,3,5,7,...n-1 odd indexes given: sum(piles) is odd, piles.length is even  
+//  If sum(piles[even]) > sum(piles[odd]), Alex just picks all evens and wins.
+// If sum(piles[even]) < sum(piles[odd]), Alex just picks all odds and wins.
 return true;
     }
-
-
 };
