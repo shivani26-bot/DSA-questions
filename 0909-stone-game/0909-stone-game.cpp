57d4d7606536bc6236ca9b383b7b229111f16b9e
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-03 12:36:55 +0530

    Time: 0 ms (100%), Space: 10.5 MB (73.28%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -22,25 +22,31 @@ public:
 
 
 //top down
-int dp[500][500][2];
-bool solve(int i, int j,int a,vector<int>&piles){
-if(i>=j) return 0;
+// int dp[500][500][2];
+// bool solve(int i, int j,int a,vector<int>&piles){
+// if(i>=j) return 0;
 
-if(dp[i][j][a]!=-1) return dp[i][j][a];
-int alice=0,bob=0;
+// if(dp[i][j][a]!=-1) return dp[i][j][a];
+// int alice=0,bob=0;
 
-if(a==1){
-    alice= max(piles[i]+solve(i+1,j,0,piles),piles[j]+solve(i,j-1,0,piles));
-}
-else{
-    bob= max(piles[i]+solve(i+1,j,1,piles),piles[j]+solve(i,j-1,1,piles));
-}
+// if(a==1){
+//     alice= max(piles[i]+solve(i+1,j,0,piles),piles[j]+solve(i,j-1,0,piles));
+// }
+// else{
+//     bob= max(piles[i]+solve(i+1,j,1,piles),piles[j]+solve(i,j-1,1,piles));
+// }
 
-return dp[i][j][a]=alice>bob;
-}
+// return dp[i][j][a]=alice>bob;
+// }
     bool stoneGame(vector<int>& piles) {
-        int n=piles.size();
-        memset(dp, -1,sizeof(dp));
-       return  solve(0,n-1,1,piles);
+    //     int n=piles.size();
+    //     memset(dp, -1,sizeof(dp));
+    //    return  solve(0,n-1,1,piles);
+
+    
+//maths
+return true;
     }
+
+
 };
