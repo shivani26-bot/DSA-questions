bb151c1cbad97a5ba2075a06096482ef1c6d5ae4
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-05 17:04:05 +0530

    Time: 370 ms (5.08%), Space: 69.1 MB (10.93%) - LeetHub

--- 1604-least-number-of-unique-integers-after-k-removals/1604-least-number-of-unique-integers-after-k-removals.cpp
+++ 1604-least-number-of-unique-integers-after-k-removals/1604-least-number-of-unique-integers-after-k-removals.cpp
@@ -22,7 +22,6 @@ public:
          }
          else{
             occurence= occurence-k;
-            // pq.push({occurence, element});
             break;
          }
         }
@@ -30,3 +29,39 @@ public:
 
     }
 };
+
+
+
+// class Solution {
+//     bool static cmp(pair<int,int>a,pair<int,int>b){
+//         return a.second>b.second;
+//     }
+// public:
+//     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+//         int n=arr.size();
+//         map<int,int>mp;
+//         for(int i=0;i<n;i++) mp[arr[i]]++;
+//         vector<pair<int,int>>p;
+//         for(auto i: mp) p.push_back({i.first,i.second});
+//         sort(p.begin(),p.end(),cmp);
+//         // for(int i=0;i<p.size();i++)
+//         // cout<<p[i].first<<" "<<p[i].second<<endl;
+//         int i=p.size()-1;
+//         while(k!=0){
+//             if(p[i].second!=0){
+//                 p[i].second--;
+//                 k--;
+//                 if(k==0 && p[i].second==0){
+//                     p.pop_back();
+//                 }
+//             }
+//             else if(p[i].second==0){
+//                 p.pop_back();
+//                 i--;
+//             }
+//             else i--;
+//         }
+//         return p.size();
+
+//     }
+// };
