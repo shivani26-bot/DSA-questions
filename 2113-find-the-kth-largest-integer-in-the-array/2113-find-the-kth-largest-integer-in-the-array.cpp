f793122c4f9c861ebcaf249ff33dfe1964e8c156
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-09 10:00:47 +0530

    Time: 67 ms (47.38%), Space: 75.9 MB (29.02%) - LeetHub

--- 2113-find-the-kth-largest-integer-in-the-array/2113-find-the-kth-largest-integer-in-the-array.cpp
+++ 2113-find-the-kth-largest-integer-in-the-array/2113-find-the-kth-largest-integer-in-the-array.cpp
@@ -2,10 +2,13 @@ class Solution {
 public:
     string kthLargestNumber(vector<string>& nums, int k) {
     // compare strings directly, because strings can be too long to fit into long long or int data types
-    auto comp= [&](string& a, string& b){
-if(a.size()!=b.size()) return a.size()>b.size();
-return a>b;
+    // "Treat shorter strings as smaller, and for same-length strings, use lexicographic order.
+    auto comp= [](string& a, string& b){
+if(a.size()!=b.size()) return a.size()>b.size();// shorter string = smaller number
+return a>b;//exicographic compare if lengths are equal
     };
+    // priority_queue, the top element is the "greatest" by default (max heap).
+// thats why we are using a custom comparator
     priority_queue<string,vector<string>,decltype(comp)>pq(comp);
     for(auto &str: nums){
         pq.push(str);
