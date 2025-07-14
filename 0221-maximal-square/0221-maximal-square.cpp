4b029a04316413a7e0ba11ebf737aaba94313217
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-14 07:24:42 +0530

    Time: 19 ms (22.74%), Space: 30.9 MB (77.14%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+// int solve(int i,int j, vector<vector<char>>& matrix){
+//       int m=matrix.size();
+//       int n=matrix[0].size();
+//       if(i>=m || j>=n) return 0;
+
+//       if(matrix[i][j]=='0') return 0;
+
+//       int right= solve(i,j+1,matrix);
+//       int bottom= solve(i+1, j,matrix);
+//       int diagonal= solve(i+1, j+1, matrix);
+//       return 1+min(right,min(bottom,diagonal));
+// }
+//     int maximalSquare(vector<vector<char>>& matrix) {
+//               int m=matrix.size();
+//       int n=matrix[0].size();
+  
+//     int maxResult=0;
+   
+//     for(int i=0;i<m;i++){ //o(m*n)
+      
+//         for(int j=0;j<n;j++){
+    
+//               if(matrix[i][j]=='1'){   
+//                  maxResult= max(maxResult,solve(i,j,matrix));
+//               }
+             
+//         }
+        
+//     }
+//    return maxResult*maxResult;
+//     }
+
+int solve(int i,int j, vector<vector<char>>& matrix, vector<vector<int>>&dp){
+      int m=matrix.size();
+      int n=matrix[0].size();
+      if(i>=m || j>=n) return 0;
+    if(dp[i][j]!=-1) return dp[i][j];
+      if(matrix[i][j]=='0') return 0;
+
+      int right= solve(i,j+1,matrix,dp);
+      int bottom= solve(i+1, j,matrix,dp);
+      int diagonal= solve(i+1, j+1, matrix,dp);
+      return dp[i][j]=1+min(right,min(bottom,diagonal));
+}
+
+    int maximalSquare(vector<vector<char>>& matrix) {
+     
+      int m=matrix.size();
+      int n=matrix[0].size();
+      vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+    int result=0;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+              if(matrix[i][j]=='1'){
+                result=max(result,solve(i,j,matrix,dp));
+              }
+        }
+    }
+   return result*result;
+    }
+};
