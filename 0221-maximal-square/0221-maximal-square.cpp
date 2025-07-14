e25a02d588fa4037c838c651550b2b3acbb40f57
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-14 08:12:41 +0530

    Time: 4 ms (86.39%), Space: 30.9 MB (58.04%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -61,30 +61,76 @@ public:
 //    return result*result;
 //     }
 
-int solve(int i,int j, vector<vector<char>>& matrix, vector<vector<int>>&dp){
-      int m=matrix.size();
-      int n=matrix[0].size();
-      if(matrix[i][j]=='0') return 0;
+// int solve(int i,int j, vector<vector<char>>& matrix, vector<vector<int>>&dp){
+//       int m=matrix.size();
+//       int n=matrix[0].size();
+//       if(matrix[i][j]=='0') return 0;
 
-      int right= dp[i][j+1];
-      int bottom= dp[i+1][j];
-      int diagonal= dp[i+1][j+1];
-      return dp[i][j]=1+min({right,bottom,diagonal});
-}
+//       int right= dp[i][j+1];
+//       int bottom= dp[i+1][j];
+//       int diagonal= dp[i+1][j+1];
+//       return dp[i][j]=1+min({right,bottom,diagonal});
+// }
 
-    int maximalSquare(vector<vector<char>>& matrix) {
+//     int maximalSquare(vector<vector<char>>& matrix) {
      
-      int m=matrix.size();
-      int n=matrix[0].size();
-      vector<vector<int>>dp(m+1,vector<int>(n+1,0));
-    int result=0;
-    for(int i=m-1;i>=0;i--){
-        for(int j=n-1;j>=0;j--){
-              if(matrix[i][j]=='1'){
-                result=max(result,solve(i,j,matrix,dp));
-              }
+    //   int m=matrix.size();
+    //   int n=matrix[0].size();
+    //   vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+//     int result=0;
+//     for(int i=m-1;i>=0;i--){
+//         for(int j=n-1;j>=0;j--){
+//               if(matrix[i][j]=='1'){
+//                 result=max(result,solve(i,j,matrix,dp));
+//               }
+//         }
+//     }
+//    return result*result;
+//     }
+
+
+
+ int maximalSquare(vector<vector<char>>& matrix) {
+            if (matrix.empty()) {
+            return 0;
         }
-    }
-   return result*result;
-    }
+
+     int m=matrix.size();
+        int n=matrix[0].size();
+        vector<vector<int>>dp(m, vector<int>(n,0));
+        int result=0;
+        // for(int j=0;j<m;j++) dp[0][j]=matrix[0][j];
+        // for(int i=0;i<n;i++) dp[i][0]=matrix[i][0];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+if(!i || !j|| matrix[i][j]=='0') dp[i][j]=matrix[i][j]-'0';
+else dp[i][j]=min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+
+result=max(result,dp[i][j]);
+            }
+         
+        }
+
+        //  for (int i = 0; i < m; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (!i || !j || matrix[i][j] == '0') {
+        //             dp[i][j] = matrix[i][j] - '0';
+        //         } else {
+        //             dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+        //         }
+        //     result = max(dp[i][j], result);
+        //     }
+        // }
+        // int sum=0;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         cout<<dp[i][j]<<" ";
+        //         cout<<endl;
+        //         sum= max(sum,dp[i][j]);
+        //     }
+        // }
+        // return sum*sum;
+        return result*result;
+ }
+      
 };
