6e265f75d4f2499844b8854ec9babccf82692c65
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-14 08:13:51 +0530

    Time: 9 ms (55.27%), Space: 31.1 MB (15.16%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -99,8 +99,7 @@ public:
         int n=matrix[0].size();
         vector<vector<int>>dp(m, vector<int>(n,0));
         int result=0;
-        // for(int j=0;j<m;j++) dp[0][j]=matrix[0][j];
-        // for(int i=0;i<n;i++) dp[i][0]=matrix[i][0];
+   
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
 if(!i || !j|| matrix[i][j]=='0') dp[i][j]=matrix[i][j]-'0';
