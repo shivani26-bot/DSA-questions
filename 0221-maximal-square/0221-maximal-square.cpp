11eff89bd0550b1af41a7bcaf728f16856b6729e
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-14 08:25:25 +0530

    Time: 11 ms (47.23%), Space: 31 MB (34.4%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -100,7 +100,7 @@ public:
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
 if(!i || !j|| matrix[i][j]=='0') dp[i][j]=matrix[i][j]-'0';
-else dp[i][j]=min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+else dp[i][j]=1+ min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) ;
 
 result=max(result,dp[i][j]);
             }
