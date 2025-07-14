054303ef00c2e98d3231fe7fc1515fdf0bfd01c7
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-14 07:36:05 +0530

    Time: 16 ms (27.13%), Space: 30.9 MB (77.14%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -32,27 +32,54 @@ public:
 //    return maxResult*maxResult;
 //     }
 
+// int solve(int i,int j, vector<vector<char>>& matrix, vector<vector<int>>&dp){
+//       int m=matrix.size();
+//       int n=matrix[0].size();
+//       if(i>=m || j>=n) return 0;
+//     if(dp[i][j]!=-1) return dp[i][j];
+//       if(matrix[i][j]=='0') return 0;
+
+//       int right= solve(i,j+1,matrix,dp);
+//       int bottom= solve(i+1, j,matrix,dp);
+//       int diagonal= solve(i+1, j+1, matrix,dp);
+//       return dp[i][j]=1+min(right,min(bottom,diagonal));
+// }
+
+//     int maximalSquare(vector<vector<char>>& matrix) {
+     
+//       int m=matrix.size();
+//       int n=matrix[0].size();
+//       vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+//     int result=0;
+//     for(int i=0;i<m;i++){
+//         for(int j=0;j<n;j++){
+//               if(matrix[i][j]=='1'){
+//                 result=max(result,solve(i,j,matrix,dp));
+//               }
+//         }
+//     }
+//    return result*result;
+//     }
+
 int solve(int i,int j, vector<vector<char>>& matrix, vector<vector<int>>&dp){
       int m=matrix.size();
       int n=matrix[0].size();
-      if(i>=m || j>=n) return 0;
-    if(dp[i][j]!=-1) return dp[i][j];
       if(matrix[i][j]=='0') return 0;
 
-      int right= solve(i,j+1,matrix,dp);
-      int bottom= solve(i+1, j,matrix,dp);
-      int diagonal= solve(i+1, j+1, matrix,dp);
-      return dp[i][j]=1+min(right,min(bottom,diagonal));
+      int right= dp[i][j+1];
+      int bottom= dp[i+1][j];
+      int diagonal= dp[i+1][j+1];
+      return dp[i][j]=1+min({right,bottom,diagonal});
 }
 
     int maximalSquare(vector<vector<char>>& matrix) {
      
       int m=matrix.size();
       int n=matrix[0].size();
-      vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+      vector<vector<int>>dp(m+1,vector<int>(n+1,0));
     int result=0;
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
+    for(int i=m-1;i>=0;i--){
+        for(int j=n-1;j>=0;j--){
               if(matrix[i][j]=='1'){
                 result=max(result,solve(i,j,matrix,dp));
               }
