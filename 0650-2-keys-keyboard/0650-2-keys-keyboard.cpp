ccff6383386df6032411b798d28d4ec2275e787a
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-15 14:23:13 +0530

    Time: 15 ms (30.4%), Space: 12.2 MB (24.36%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -25,23 +25,39 @@ public:
 //     }
 
 
-int solve(int currA, int clipA, int n){
-    if(currA==n) return 0;//no operation required further
-    if(currA>n) return 100000; //invalid Case
-    // when we copyAll then how many A's will come in the clip board other than already existing
-    int copyAllPaste= 2+solve(currA+currA,currA,n);//2operations
-    // here we are not copying anything hence clipA remains same 
-int paste= 1+solve(currA+clipA,clipA,n);//one operation
+// int solve(int currA, int clipA, int n){
+//     if(currA==n) return 0;//no operation required further
+//     if(currA>n) return 100000; //invalid Case
+//     // when we copyAll then how many A's will come in the clip board other than already existing
+//     int copyAllPaste= 2+solve(currA+currA,currA,n);//2operations
+//     // here we are not copying anything hence clipA remains same 
+// int paste= 1+solve(currA+clipA,clipA,n);//one operation
 
-return min(copyAllPaste,paste);
+// return min(copyAllPaste,paste);
+// }
+//     int minSteps(int n) {
+//     //    we have two possibility , copyall+paste and paste
+//     //    take the minimum of both
+//     // initially we have to copy for the first time to make further operations possible
+// // maintain currA value and clipBoard value
+// if(n==1) return 0; //we already have 1 'A' in the notepad
+// // number of A's currently , number of A on clip board,
+//    return 1+solve(1,1,n);
+//     }
+
+
+int dp[1001][1001];
+int solve(int currA, int clipA, int n){
+    if(currA==n) return 0;
+    if(currA>n) return 100000; 
+    if(dp[currA][clipA]!=-1) return dp[currA][clipA];
+    int copyAllPaste= 2+solve(currA+currA,currA,n);
+    int paste= 1+solve(currA+clipA,clipA,n);
+    return dp[currA][clipA]=min(copyAllPaste,paste);
 }
     int minSteps(int n) {
-    //    we have two possibility , copyall+paste and paste
-    //    take the minimum of both
-    // initially we have to copy for the first time to make further operations possible
-// maintain currA value and clipBoard value
-if(n==1) return 0; //we already have 1 'A' in the notepad
-// number of A's currently , number of A on clip board,
+   if(n==1) return 0;
+   memset(dp,-1,sizeof(dp));
    return 1+solve(1,1,n);
     }
 
