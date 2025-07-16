a4e8e83220dbff17d948ef6ee52ce8b788f34af2
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-16 10:48:24 +0530

    Time: 255 ms (48%), Space: 157 MB (52.73%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
@@ -30,6 +30,26 @@ int n;
 
 
 //o(2*n) only these many states will be visited
+// long long solve(int idx , bool isEven,vector<int>& nums,vector<vector<long long>>&dp){
+// if(idx>=n) return 0;
+// if(dp[idx][isEven]!=-1) return dp[idx][isEven];
+//     long long take=0;
+//     if(isEven){
+//         take=solve(idx+1,!isEven,nums,dp)+nums[idx];
+//     }
+//     else{
+//         take=solve(idx+1,!isEven,nums,dp)-nums[idx];
+//     }
+//     long long notTake= solve(idx+1, isEven,nums,dp);
+
+//     return dp[idx][isEven]=max(take,notTake);
+// }
+//     long long maxAlternatingSum(vector<int>& nums) {
+// n=nums.size();
+// vector<vector<long long>>dp(n,vector<long long>(2,-1));
+// return solve(0,true,nums,dp);
+//     }
+
 long long solve(int idx , bool isEven,vector<int>& nums,vector<vector<long long>>&dp){
 if(idx>=n) return 0;
 if(dp[idx][isEven]!=-1) return dp[idx][isEven];
@@ -46,7 +66,22 @@ if(dp[idx][isEven]!=-1) return dp[idx][isEven];
 }
     long long maxAlternatingSum(vector<int>& nums) {
 n=nums.size();
-vector<vector<long long>>dp(n,vector<long long>(2,-1));
-return solve(0,true,nums,dp);
+vector<vector<long long>>dp(n+1,vector<long long>(2,0));
+for(int idx=n-1;idx>=0;idx--){
+    for(int isEven=0;isEven<2;isEven++){
+long long take=0;
+    if(isEven){
+        take=dp[idx+1][!isEven]+nums[idx];
+    }
+    else{
+        take=dp[idx+1][!isEven]-nums[idx];
+    }
+    long long notTake= dp[idx+1][isEven];
+
+    dp[idx][isEven]=max(take,notTake);
+    }
+    
+}
+ return dp[0][1];
     }
 };
