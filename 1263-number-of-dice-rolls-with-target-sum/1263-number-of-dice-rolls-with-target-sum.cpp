17421f16410f089fd0c161fe570f599af18ee7b6
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-17 06:47:55 +0530

    Time: 32 ms (7.22%), Space: 11.4 MB (35.1%) - LeetHub

--- 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.cpp
@@ -27,20 +27,43 @@ public:
 //     }
 
 
-int mod=1e9+7;
-int solve(int n, int k, int target,vector<vector<int>>&dp){
-if(target<0) return 0;
-if(n==0) return target==0;
-if(dp[n][target]!=-1) return dp[n][target];
-int countWays=0;
-    for(int i=1;i<=k;i++){
-        countWays=(countWays%mod+solve(n-1,k, target-i,dp)%mod)%mod;
-    }
-    return dp[n][target]=countWays;
-}
-    int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
-return solve(n,k,target,dp);
+// int mod=1e9+7;
+// int solve(int n, int k, int target,vector<vector<int>>&dp){
+// if(target<0) return 0;
+// if(n==0) return target==0;
+// if(dp[n][target]!=-1) return dp[n][target];
+// int countWays=0;
+//     for(int i=1;i<=k;i++){
+//         countWays=(countWays%mod+solve(n-1,k, target-i,dp)%mod)%mod;
+//     }
+//     return dp[n][target]=countWays;
+// }
+//     int numRollsToTarget(int n, int k, int target) {
+//         vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
+// return solve(n,k,target,dp);
 
+//     }
+
+
+
+
+int mod=1e9+7;
+
+    int numRollsToTarget(int n, int k, int target) {
+        vector<vector<int>>dp(n+1,vector<int>(target+1,0));
+        dp[0][0]=1;
+// if n==0 then for target 1 , 2,3 ,.... it will be 0
+// if target==0 then for n=1,2,3,4,5...it will be 0
+        for(int dice=1; dice<n+1; dice++){
+            for(int t=1 ;t<target+1;t++){
+                int countWays=0;
+                for(int i=1;i<=k;i++){//faces
+                if(t-i>=0) //t>=i
+        countWays=(countWays%mod+dp[dice-1][t-i]%mod)%mod;
+    }
+    dp[dice][t]=countWays;
+            }
+        }
+return dp[n][target];
     }
 };
