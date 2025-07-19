49225ceb6962c8d78f244716ca16e9569200428b
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-19 13:00:40 +0530

    Time: 231 ms (5.37%), Space: 90.8 MB (14.64%) - LeetHub

--- 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.cpp
+++ 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-// int n;
+int n;
 // maintain currIdx, prevElement
 // if(s[currIdx]=='0'){
 //     2 options
@@ -30,7 +30,7 @@ public:
 //          notflip=solve(i+1,s[i])
 //     }
 // }
-// int solve(int currIdx,int prev,string s){
+// int solve(int currIdx,int prev,string &s){
 
 // if(currIdx>=n) return 0;
 // int flip=INT_MAX;int notflip=INT_MAX;
@@ -56,81 +56,36 @@ public:
 // }
 //     int minFlipsMonoIncr(string s) {
 //         n=s.length();
-//         int startIdx;
 //         return solve(0,0,s);
 //     }
-// int solve(int currIdx,int prev,string &s,vector<vector<int>>dp){
+int solve(int currIdx,int prev,string &s,vector<vector<int>>&dp){
 
-// if(currIdx>=n) return 0;
-// if(dp[currIdx][prev]!=-1) return dp[currIdx][prev];
-// int flip=INT_MAX;int notflip=INT_MAX;
-// if(s[currIdx]=='0'){
-//     if(prev==0){
-//         flip=1+solve(currIdx+1,1,s,dp);
-//         notflip=solve(currIdx+1,0,s,dp);
-//     }
-//     else if(prev==1){
-//         flip=1+solve(currIdx+1,1,s,dp);
-//     }
-// }
-// else{//currIdx=='1'
-//     if(prev==0){
-//           flip=1+solve(currIdx+1,0,s,dp);
-//         notflip=solve(currIdx+1,1,s,dp);
-//     }
-//     else if(prev==1){
-//          notflip=solve(currIdx+1,1,s,dp);
-//     }
-// }
-//     return dp[currIdx][prev]=min(flip,notflip);
-// }
-//     int minFlipsMonoIncr(string s) {
-//         n=s.length();
-//         int startIdx;
-//         vector<vector<int>>dp(n+1,vector<int>(2,-1));
-//         return solve(0,0,s,dp);
-//     }
-
-
- int n;
-    
-    int solve(string &s, int curr_index, int prev_val, vector<vector<int>>& t) {
-        if(curr_index >= n)
-            return 0;
-        
-        if(t[curr_index][prev_val] != -1)
-            return t[curr_index][prev_val];
-        
-        int flip    = INT_MAX;
-        int no_flip = INT_MAX;
-
-        if(s[curr_index] == '0') {
-            
-            if(prev_val == 1) {
-                flip = 1 + solve(s, curr_index+1, 1, t);
-            } else {
-                flip = 1 + solve(s, curr_index+1, 1, t);
-                no_flip = solve(s, curr_index+1, 0, t);
-            }
-        }
-
-        else if(s[curr_index] == '1')
-        {
-            if(prev_val == 1) {
-                no_flip = solve(s, curr_index+1, 1, t);
-            } else {
-                flip    = 1 + solve(s, curr_index+1, 0, t);
-                no_flip = solve(s, curr_index+1, 1, t);
-            }
-        }
-     return t[curr_index][prev_val] =  min(flip, no_flip);
+if(currIdx>=n) return 0;
+if(dp[currIdx][prev]!=-1) return dp[currIdx][prev];
+int flip=INT_MAX;int notflip=INT_MAX;
+if(s[currIdx]=='0'){
+    if(prev==0){
+        flip=1+solve(currIdx+1,1,s,dp);
+        notflip=solve(currIdx+1,0,s,dp);
     }
-    
+    else if(prev==1){
+        flip=1+solve(currIdx+1,1,s,dp);
+    }
+}
+else{//currIdx=='1'
+    if(prev==0){
+          flip=1+solve(currIdx+1,0,s,dp);
+        notflip=solve(currIdx+1,1,s,dp);
+    }
+    else if(prev==1){
+         notflip=solve(currIdx+1,1,s,dp);
+    }
+}
+    return dp[currIdx][prev]=min(flip,notflip);
+}
     int minFlipsMonoIncr(string s) {
-        n = s.length();
-        
-        vector<vector<int>> t(n+1, vector<int>(2, -1));
-        
-        return solve(s, 0, 0, t);
+        n=s.length();
+        vector<vector<int>>dp(n+1,vector<int>(2,-1));
+        return solve(0,0,s,dp);
     }
 };
