6a7c7aeebf49b46276f7d127abd9d0ba80b9e549
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-24 12:17:16 +0530

    Time: 75 ms (56.29%), Space: 94.5 MB (69.03%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -4,13 +4,19 @@ public:
         
 int n=nums.size();
         int i=0,j=0;
+        unordered_map<int,int>mp;//occurence of each number in a window
+if(k<n) k=k+1;
+else k=n;
         while(j<n){
-        if(i<j && nums[i]==nums[j]) return true;
-            if(j-i<k) j++;
-            else{
-                i++;
-        //   j++;
-            }
+            mp[nums[j]]++;
+            if(j-i+1<k) j++;
+            else if(j-i+1==k){
+             if(mp.size()<k) return true;
+             mp[nums[i]]--;
+             if(mp[nums[i]]==0) mp.erase(nums[i]);
+             i++;
+             j++;
+          }  
         }
         return false;
     }
