02496fa30fadbaf1845c35cdefae1ed540293542
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-24 15:04:49 +0530

    Time: 78 ms (41.11%), Space: 94.5 MB (69.11%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {  
 int n=nums.size();
         int i=0,j=0;
         unordered_map<int,int>mp;//occurence of each number in a window
         // the adjustment k = k + 1 when k is less than the size of the nums array ensures that the sliding window size is at least k.
 if(k<n) k=k+1;
 else k=n;
+
         while(j<n){
             mp[nums[j]]++;
             if(j-i+1<k) j++;
