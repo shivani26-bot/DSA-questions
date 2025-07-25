9bd3a63382e5065e78c608fb71b434a91157d926
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-25 07:39:09 +0530

    Time: 4 ms (18.21%), Space: 97.5 MB (25.62%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,7 +5,7 @@ public:
         int minPrice=INT_MAX;
         int ans=INT_MIN;
         for(int i=0;i<n;i++){
-            minPrice=min(minPrice,prices[i]);
+            minPrice=min(minPrice,prices[i]);//min price of stock till current index
             ans=max(ans,prices[i]-minPrice);
         }
         return ans;//maxprofit
