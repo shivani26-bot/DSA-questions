3d0533b46f955193eff0ca9b3e8f9d67cb9cf1ed
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-07-26 16:31:21 +0530

    Time: 73 ms (47.84%), Space: 102.6 MB (93.57%) - LeetHub

---
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        unordered_map<int,int>mp;
+
+        int i=0,j=1,n=colors.size();
+        int badPairs=0;
+        int alternateGroup=0;
+        while(i<n){
+            if(colors[j%n]==colors[(j-1)%n]) badPairs++;
+            if(j-i+1<k) j++;
+            else if(j-i+1==k){
+               if(badPairs==0) alternateGroup++;
+              if(colors[i%n]==colors[(i+1)%n]) badPairs--;
+                i++;
+                //  if(colors[i%n]==colors[(i-1)%n]) badPairs--;
+                j++;
+            }
+        }
+        return alternateGroup;
+    }
+};
