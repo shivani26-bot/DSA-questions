cc1897ca32cab52cae1f09c0b3b4ab610c75b609
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-08-06 08:37:44 +0530

    Time: 40 ms (50.74%), Space: 105.8 MB (90.94%) - LeetHub

--- 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
@@ -1,3 +1,32 @@
+// class Solution {
+// public:
+//     int maxFrequency(vector<int>& nums, int k) {
+//         int i=0,j=0, n=nums.size();
+//         long currSum=0;
+//         sort(nums.begin(),nums.end());//sorting such that always the last element of the window will be max element
+        
+//         long requiredTotalInWin=0;
+//         int maxWin=INT_MIN;
+//         while(j<n){
+//             currSum+=nums[j];
+
+//             long maxEleInWin=nums[j];
+//             requiredTotalInWin= maxEleInWin*(j-i+1);
+   
+//         if(requiredTotalInWin-currSum>k){
+                
+//                 currSum-=nums[i];
+//                 i++;
+           
+//             }
+//             maxWin= max(maxWin, j-i+1);
+//             j++;
+
+//         }
+//         return maxWin;
+//     }
+// };
+
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
@@ -5,15 +34,15 @@ public:
         long currSum=0;
         sort(nums.begin(),nums.end());//sorting such that always the last element of the window will be max element
         
-        long requiredTotalInWin=0;
+
         int maxWin=INT_MIN;
         while(j<n){
             currSum+=nums[j];
 
             long maxEleInWin=nums[j];
-            requiredTotalInWin= maxEleInWin*(j-i+1);
+           
    
-        if(requiredTotalInWin-currSum>k){
+        while(maxEleInWin*(j-i+1)-currSum>k){
                 
                 currSum-=nums[i];
                 i++;
