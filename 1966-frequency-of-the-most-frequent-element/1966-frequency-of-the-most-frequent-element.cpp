5c5cdc752b2c88c3b5fa3bdaebdcf56f6a2656fa
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-08-06 08:32:53 +0530

    Time: 51 ms (16.89%), Space: 105.8 MB (59.97%) - LeetHub

---
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        int i=0,j=0, n=nums.size();
+        long currSum=0;
+        sort(nums.begin(),nums.end());//sorting such that always the last element of the window will be max element
+        
+        long requiredTotalInWin=0;
+        int maxWin=INT_MIN;
+        while(j<n){
+            currSum+=nums[j];
+
+            long maxEleInWin=nums[j];
+            requiredTotalInWin= maxEleInWin*(j-i+1);
+   
+        if(requiredTotalInWin-currSum>k){
+                
+                currSum-=nums[i];
+                i++;
+           
+            }
+            maxWin= max(maxWin, j-i+1);
+            j++;
+
+        }
+        return maxWin;
+    }
+};
