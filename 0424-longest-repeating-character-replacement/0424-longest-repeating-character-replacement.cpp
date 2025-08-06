397b50ef5676d2582a34a00344ba6958f0e4a5e3
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-08-06 08:36:27 +0530

    Time: 8 ms (37.92%), Space: 11.2 MB (5.3%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,22 +1,85 @@
+// class Solution {
+// public:
+//     int characterReplacement(string s, int k) {
+// //         int n=s.length();
+// //         int i=0,j=0;
+// //         unordered_map<char,int>mp;
+// // int maxWin=INT_MIN;
+// // int maxRepeatCharInWin=0;
+// //         while(j<n){
+// //             mp[s[j]]++;
+// //             int currLen=j-i+1;
+// //             maxRepeatCharInWin=max(mp[s[j]],maxRepeatCharInWin);
+// //             if(abs(currLen-maxRepeatCharInWin)>k){
+// //                 mp[s[i]]--;
+// //                 if(mp[s[i]]==0) mp.erase(s[i]);
+// //                 i++;
+// //             }
+// //             currLen=j-i+1;
+// //             maxWin=max(maxWin,currLen);
+// //             j++;
+// //         }
+// //         return maxWin;
+//        int n=s.length();
+//         int i=0,j=0;
+//         unordered_map<char,int>mp;
+// int maxWin=INT_MIN;
+// int maxRepeatCharInWin=0;
+//         while(j<n){
+//             mp[s[j]]++;
+//             int currLen=j-i+1;
+//             maxRepeatCharInWin=max(mp[s[j]],maxRepeatCharInWin);
+//             if(abs(currLen-maxRepeatCharInWin)>k){
+//                 mp[s[i]]--;
+//                 if(mp[s[i]]==0) mp.erase(s[i]);
+//                 i++;
+//             }
+//             currLen=j-i+1;
+//             maxWin=max(maxWin,currLen);
+//             j++;
+//         }
+//         return maxWin;
+//     }
+// };
+
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int n=s.length();
+//         int n=s.length();
+//         int i=0,j=0;
+//         unordered_map<char,int>mp;
+// int maxWin=INT_MIN;
+// int maxRepeatCharInWin=0;
+//         while(j<n){
+//             mp[s[j]]++;
+//             int currLen=j-i+1;
+//             maxRepeatCharInWin=max(mp[s[j]],maxRepeatCharInWin);
+//             if(abs(currLen-maxRepeatCharInWin)>k){
+//                 mp[s[i]]--;
+//                 if(mp[s[i]]==0) mp.erase(s[i]);
+//                 i++;
+//             }
+//             currLen=j-i+1;
+//             maxWin=max(maxWin,currLen);
+//             j++;
+//         }
+//         return maxWin;
+       int n=s.length();
         int i=0,j=0;
         unordered_map<char,int>mp;
 int maxWin=INT_MIN;
 int maxRepeatCharInWin=0;
         while(j<n){
             mp[s[j]]++;
-            int currLen=j-i+1;
+
             maxRepeatCharInWin=max(mp[s[j]],maxRepeatCharInWin);
-            if(abs(currLen-maxRepeatCharInWin)>k){
+            while(abs((j-i+1)-maxRepeatCharInWin)>k){
                 mp[s[i]]--;
                 if(mp[s[i]]==0) mp.erase(s[i]);
                 i++;
             }
-            currLen=j-i+1;
-            maxWin=max(maxWin,currLen);
+ 
+            maxWin=max(maxWin,j-i+1);
             j++;
         }
         return maxWin;
