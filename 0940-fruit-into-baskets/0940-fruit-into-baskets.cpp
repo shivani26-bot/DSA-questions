1245d8b91d702b32bb36bdffeff3a6f9c83d3022
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-08-06 08:57:38 +0530

    Time: 212 ms (5.16%), Space: 71.1 MB (88.83%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    int totalFruit(vector<int>& fruits) {
-        int n=fruits.size();
-        int i=0,j=0;
-        unordered_map<int,int>mp;
-        int maxWin=INT_MIN;
-        while(j<n){
-            mp[fruits[j]]++;
-            // sum+=fruits[j];
-while(mp.size()>2){
-    mp[fruits[i]]--;
-if(mp[fruits[i]]==0) mp.erase(fruits[i]);
-i++;
-   
-}
-
- maxWin=max(maxWin, j-i+1);
-    j++;
+    int totalFruit(vector<int>& s) {
+      int i=0,j=0;
+      unordered_map<int,int>mp;
+      int n=s.size();
+      int maxwindow=INT_MIN;
+      while(j<n){
+          mp[s[j]]++;
+          if(mp.size()>2){
+              while(mp.size()>2)
+              {
+                  mp[s[i]]--;
+                  if(mp[s[i]]==0) mp.erase(s[i]);
+                  i++;
+              }
+              j++;
+          }
+          else if(mp.size()<=2){
+              maxwindow= max(maxwindow,j-i+1);
+              j++;
+          }
         }
-        return maxWin;
+        return maxwindow;
     }
 };
