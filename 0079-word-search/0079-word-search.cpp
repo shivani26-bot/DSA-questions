0661822b772071ecb5ca3a28be555ecad0166113
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-08-16 12:44:56 +0530

    Time: 1234 ms (8.45%), Space: 11.8 MB (17.16%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-bool isSafe(int row, int col, int n, int m,vector<vector<bool>>& visited,string word, int currindex, vector<vector<char>>&board ){
-    return row>=0 && row<n && col>=0 && col<m && !visited[row][col] && board[row][col]==word[currindex];
-}
-bool wordExist(vector<vector<char>>&board, string word,int n, int m, vector<vector<bool>>& visited, int i, int j, int currindex){
-// i -> row
+int n, m;
+bool isSafe(int i, int j, vector<vector<char>>& board,string word,vector<vector<bool>>& visited,int currIdx){
+    // i -> row
 // j -> col
-  if(currindex>=word.length()) return true;
-  
-  if(!isSafe(i,j,n,m,visited,word,currindex,board)){
-         return false;
-    }
 
-    visited[i][j]=true;
-   bool wordFound =wordExist(board,word,n,m,visited,i,j+1,currindex+1) ||
-                   wordExist(board,word,n,m,visited,i,j-1,currindex+1) || 
-                   wordExist(board,word,n,m,visited,i+1,j,currindex+1) ||
-                   wordExist(board,word,n,m,visited,i-1,j,currindex+1);
-    visited[i][j]=false;
+    return i>=0 && i<n && j>=0 && j<m && !visited[i][j] && board[i][j]==word[currIdx];
+}
+bool solve(int i, int j, vector<vector<char>>& board,string word,vector<vector<bool>>& visited,int currIdx){
+
+if(currIdx>=word.length()) return true;
+if(!isSafe(i,j,board,word,visited,currIdx)) return false;
+visited[i][j]=true;
+
+bool wordFound= solve(i,j-1,board,word,visited, currIdx+1) || solve(i,j+1,board,word,visited, currIdx+1) ||solve(i-1,j,board,word,visited, currIdx+1) ||solve(i+1,j,board,word, visited,currIdx+1);
+visited[i][j]=false;
+
     return wordFound;
 }
-
     bool exist(vector<vector<char>>& board, string word) {
-        int n=board.size();
-        int m=board[0].size();
-        vector<vector<bool>> visited(n,vector<bool>(m,false));
-      for(int i=0;i<n;i++){
+       n=board.size();
+       m=board[0].size();
+      vector<vector<bool>>visited(n,vector<bool>(m,false));
+       for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            if(board[i][j]==word[0] && wordExist(board,word, n, m, visited,i,j,0))
-            return true;
+            if(word[0]==board[i][j] && solve(i,j,board,word,visited,0)){
+                return true;
+            }
         }
-      }
-      return false;
+       }
+       return false; 
     }
 };
+
 // O(n*m*4^k)	The function wordExist is recursively called for each cell in the board, and at each call, it explores up to 4 neighboring cells. 'k' represents the length of the word being searched for
