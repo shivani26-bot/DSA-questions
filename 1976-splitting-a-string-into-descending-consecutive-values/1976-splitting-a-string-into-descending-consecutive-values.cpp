2dd989a2588bcf6186b00c9abba624ac8eeb8fba
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-08-19 12:08:42 +0530

    Time: 3 ms (30.11%), Space: 9.1 MB (51.7%) - LeetHub

---
+++ 1976-splitting-a-string-into-descending-consecutive-values/1976-splitting-a-string-into-descending-consecutive-values.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+int n;
+
+const long long MAX=999999999999;
+bool solve(string s, int idx,long long prev,int count,vector<string>&temp){
+    if (idx == n) {
+        // print one valid sequence when string is completely split
+        cout << "Sequence: ";
+        for (auto &ele : temp) cout << ele << " ";
+        return count>1;//there must be more that 1 elements to form decreasing order
+    }
+long long num=0;
+    for(int j=idx;j<n;j++){
+        num=num*10+s[j]-'0';
+        if(num>MAX) break;
+         //must be strictly decreasing
+        if(prev!=-1 && num>=prev) break;
+        string subs= s.substr(idx,j-idx+1);
+        if(prev==-1 || prev-num==1){
+            temp.push_back(subs);
+if(solve(s,j+1,num,count+1,temp)) return true;
+temp.pop_back();
+        }
+       
+    }
+   return false;
+}
+    bool splitString(string s) {
+        n=s.length();
+        if(n<=1) return false;
+        vector<string>temp;
+  return solve(s,0,-1,0,temp);
+ 
+    
+    }
+};
