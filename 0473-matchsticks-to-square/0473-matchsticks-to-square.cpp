7e56e57c017cd3c5ae94c6a801bbf88cad2a5935
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-08-19 10:19:08 +0530

    Time: 51 ms (70.21%), Space: 12.7 MB (60.71%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
@@ -5,6 +5,10 @@ int n;
 bool solve(vector<int>& matchsticks, vector<int>&visited,int target, int idx, int curr_sum, int k){
   if(k==1) return true;
   if(idx>=n) return false;
+//   You just finished forming one complete side.
+// Now you need to start filling the next side (k-1 sides left).
+// When starting a new side, you shouldn’t restrict yourself to continuing from the last index idx+1 → you need to consider all sticks again (except the ones already marked visited).
+// That’s why recursion resets idx=0.
        if(curr_sum==target) return solve(matchsticks,visited,target,0,0,k-1) ;
 
     for(int j=idx;j<n;j++){
@@ -26,6 +30,8 @@ if(visited[j] || curr_sum+matchsticks[j]>target) continue;
     if(n<4 ||  totalSum%4) return false;
     int target= totalSum/4;
     vector<int>visited(n,false);
+    // Backtracking explores choices. If we place larger sticks first, we quickly see whether a configuration is impossible.
+    // Without sorting, the recursion might waste time trying many small sticks before realizing that a big stick won’t fit late
      sort( matchsticks.begin(), matchsticks.end(),greater<int>());
 return solve(matchsticks,visited,target,0,0,4);
     }
