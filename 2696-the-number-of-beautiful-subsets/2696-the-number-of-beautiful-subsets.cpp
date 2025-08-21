5094e388c7c0fc652cd108adea529c4ae0c82c07
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-08-21 08:43:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2696-the-number-of-beautiful-subsets/2696-the-number-of-beautiful-subsets.cpp
+++ 2696-the-number-of-beautiful-subsets/2696-the-number-of-beautiful-subsets.cpp
@@ -3,32 +3,18 @@ public:
 int n;
 
 vector<vector<int>>result;
-void solve(vector<int>& nums, int k, vector<int>temp, int idx, int prev){
-if(idx>=n){
-    // result.push_back(temp);
-    return;
-}
-    for(int j=idx;j<n;j++){
-        if(prev==-1 || (nums[j]-prev!=k)){
-            temp.push_back(nums[j]);
-            result.push_back(temp);
-            solve(nums,k, temp,j+1,nums[j]);
-            temp.pop_back();
-        }
-    }
+int solve(vector<int>& nums, int k,  int idx, int prev){
+
+if(idx==n) return 0;
+int pick=0;
+int notPick= solve(nums,k,idx+1,prev);
+if(prev==-1 || abs(nums[idx]-nums[prev])!=k)
+pick=1+solve(nums,k,idx+1,idx);
+return pick+notPick;
 }
     int beautifulSubsets(vector<int>& nums, int k) {
       n=nums.size();
-      vector<int>temp;
       sort(nums.begin(),nums.end());
-      solve(nums,k,temp,0,-1); 
-      for(auto& vec:result){
-        cout<<"[";
-        for(auto& num:vec){
-            cout<<num<<",";
-        }
-        cout<<"]";
-      } 
-      return result.size();
+      return solve(nums,k,0,-1); 
     }
 };
