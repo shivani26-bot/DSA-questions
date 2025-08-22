4b4b14eacff876cd71a1fe63bf55e44550c6bb4d
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-08-22 09:53:44 +0530

    Time: 12 ms (44.42%), Space: 10.5 MB (97.09%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -19,3 +19,48 @@ return take+notTake;
         return solve(nums,0,0,maxOr);
     }
 };
+
+
+// class Solution {
+// public:
+// // n*2^n
+//     int countMaxOrSubsets(vector<int>& nums) {
+//             int n=nums.size();
+		    
+// int subsets=(1<<n) ;///2^n subsets
+// int x=0;
+// int max_or=INT_MIN;
+// vector<int>maxm_or;
+// unordered_map<int,int>mp;
+// 		    for(int num=1;num<(1<<n);num++){ //o(2^n-1)
+// 		  x=0;
+// 		        for(int i=0;i<n;i++){ //o(n)
+                  
+// 		            if(num&(1<<i))
+// 		            x|=nums[i];
+		            
+// 		        }
+// 		        cout<<x<<endl;
+//                 if(x>max_or){ //o(1)
+//                     max_or=x;
+//                     // cout<<max_or<<endl;
+//                     maxm_or.push_back(max_or);
+//                 }
+//                 // max_or=max(max_or, x);
+//                 // mp[max_or]++;
+//                 mp[x]++; //o(1)
+//                 // cout<<max_or<<endl;
+// 		    }
+//             //o(n)
+//             int maxm=*max_element(maxm_or.begin(),maxm_or.end());
+//         // cout<<maxm<<endl;
+// 	      return mp[maxm]; //o(1)
+// 		    // return 0;
+//     }
+// };
+
+// //Acceptable Time Complexities
+// //O(n!)
+// //O(2^n) or \U0001d442(3^\U0001d45b) are acceptable since 
+// // 2^16=65536and 3^16≈43×10^6 which are feasible for typical competitive programming limits.
+// // O(n^2),O(nlogn), O(n)
