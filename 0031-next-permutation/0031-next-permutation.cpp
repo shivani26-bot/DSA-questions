0161539d34e85b57b274a8722dd731aad404e840
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-08-23 11:10:00 +0530

    Time: 0 ms (100%), Space: 15.8 MB (33.11%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -17,12 +17,18 @@ public:
         // }
         // reverse(nums.begin() + i + 1, nums.end());  
 
+// [1,2,3]=>lexicographical permutation
+// [1,2,3] [1,3,2] [2,1,3] [2,3,1] [3,1,2] [3,2,1] , no next permutation for [3,2,1] return the first array :[1,2,3]
 
+// for current element find the largest to it's right swap with it and reverse the right part 
+
+// first find the candidate from the right side where nums[i]>nums[i-1]
+// [1,3,4,5,2]=> here 4 is the candidate, 5 is just greater than 4 , [1 3 5 2 4]
         int i=nums.size()-1;
-        while(i>0&&nums[i]<=nums[i-1])
+        while(i>0&&nums[i]<=nums[i-1]) //choose i-1 as candidate to swap
             i--;
         if(i-1>=0){
-            
+            //swap the element on index i, with the largest element on the right
             for(int j=nums.size()-1;j>=i;j--){
                 if(nums[j]>nums[i-1]){
                     swap(nums[j],nums[i-1]);
@@ -31,6 +37,8 @@ public:
                 }
             }
         }
+
+        //reverse the right part
         int l=nums.size()-1, k= i;
         while(k<l){
             swap(nums[k],nums[l]);
