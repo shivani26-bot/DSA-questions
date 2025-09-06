c5bfe60490fca9feea34092459c5bd4d34ea600b
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-09-06 11:17:19 +0530

    Time: 0 ms (100%), Space: 15 MB (24.78%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -10,33 +10,54 @@
  */
 class Solution {
 public:
-int llLength(ListNode* head){
-    int count=0;
-    while(head){
-        count++;
-        head=head->next;
-    }
-    return count;
-}
+// int llLength(ListNode* head){
+//     int count=0;
+//     while(head){
+//         count++;
+//         head=head->next;
+//     }
+//     return count;
+// }
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int length=llLength(head);
-        int pos=length-n;
-        ListNode* curr=head;
-        if(pos-1<0){
-            return head->next;
-        }
-        while(pos-1>0){
-            curr=curr->next;
-            pos--;
-        }
+        // int length=llLength(head);
+        // int pos=length-n;
+        // ListNode* curr=head;
+        // if(pos-1<0){
+        //     return head->next;
+        // }
+        // while(pos-1>0){
+        //     curr=curr->next;
+        //     pos--;
+        // }
         
-        ListNode* next=curr->next->next;
-        ListNode* prev=curr;
-        curr=curr->next;
-        prev->next=next;
-        delete(curr);
-        return head;
+        // ListNode* next=curr->next->next;
+        // ListNode* prev=curr;
+        // curr=curr->next;
+        // prev->next=next;
+        // delete(curr);
+        // return head;
 
-
+ListNode* p1=head;
+ListNode* p2=head;
+int count=0;
+while(count<n){
+    p1=p1->next;
+    count++;
+}
+// p2 always points to the node before the deleted node position 
+if(!p1){
+    ListNode* temp=head->next;
+    delete(head);
+    return temp;
+}
+// if n-1th node is to be deleted from the end then p1->next will be null 
+while(p1->next){
+p1=p1->next;
+p2=p2->next;
+}
+ListNode* curr=p2->next;
+p2->next=curr->next;
+delete(curr);
+return head;
     }
 };
