0ef240718aac80b43243072089c2f43f60e541fd
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-09-10 09:41:28 +0530

    Time: 0 ms (100%), Space: 16.3 MB (64.11%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || k==0) return head;
+// count total nodes in the ll and find the tail so that we can form a cycle by joining the tail to head
+        int lengthLL=1;
+        ListNode* tail=head;
+        while(tail->next){
+            lengthLL++;
+            tail=tail->next;
+        }
+
+// k>n then k must always be k=k%n
+        k=k%lengthLL;
+        if(k==0) return head;
+        
+// now join the tail to head to form the cycle
+tail->next=head;
+
+        
+        // find how many steps to reach the new tail
+        int stepsToNewHead= lengthLL-k;
+        ListNode* newTail=tail;
+        while(stepsToNewHead--){
+            newTail=newTail->next;
+        }
+        
+ListNode* newHead=newTail->next;
+newTail->next=NULL;
+return newHead;
+   
+    }
+};
