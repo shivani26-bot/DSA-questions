877c80bc37676d96a7fcfb8bd25ebd664333af56
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-09-12 08:35:36 +0530

    Time: 5 ms (55.79%), Space: 64.9 MB (90.07%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         // we can use binary search 
-        int n=nums.size();
-        int low=1;
-        int high=n;
-        while(low<=high){
-            int mid=(low+high)/2;
-            int countEle=0;
-            for(auto it:nums){
-                if(it<=mid) countEle++;
+        // int n=nums.size();
+        // int low=1;
+        // int high=n;
+        // while(low<=high){
+        //     int mid=(low+high)/2;
+        //     int countEle=0;
+        //     for(auto it:nums){
+        //         if(it<=mid) countEle++;
 
-            }
-            if(countEle<=mid) low=mid+1;
-            else high=mid-1;
-        }
-        return low;
+        //     }
+        //     if(countEle<=mid) low=mid+1;
+        //     else high=mid-1;
+        // }
+        // return low;
 
 //         Initialize two pointers, slow and fast, to the first element of the input list nums.
 // Iterate through the list using the Floyd's Tortoise and Hare algorithm(See below) to find a cycle.
