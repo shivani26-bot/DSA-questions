488e0edd9123fba2838f169cb73504bff78732ae
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-09-15 08:45:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>>ans;
+        int i=0;
+        int n=intervals.size();
+        // if new interval starting point is greater than current interval end point it means they are separate and keep inserting the current interval 
+        while(i<n && intervals[i][1]<newInterval[0]){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+        // buut as soon as the new interval overlaps with current intervals, now find the merged intervals start and end point
+        int st=newInterval[0],en=newInterval[1];
+        vector<int>mergedInterval={st,end};
+        // now check till current intervals first index is less than en
+        while(i<n && intervals[i][0]<=en){
+            mergedInterval[0]=min(mergedInterval[0],intervals[i][0]);
+             mergedInterval[1]=max(mergedInterval[1],intervals[i][1]);
+        }
+    }
+    ans.push_back(mergedInterval);
+    // now push the remaining elements 
+    while(i<n){
+        ans.push_back(intervals[i]);
+        i++;
+    }
+    return ans;
+};
