c56752891d6a057046a87d4b957271179aa6eb46
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-09-21 16:24:54 +0530

    Time: 5 ms (98.47%), Space: 39.4 MB (94.59%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.cpp
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -2,36 +2,40 @@ class MyCalendarTwo {
 public:
 vector<pair<int,int>>bookings;
 vector<pair<int,int>>overlappings;
+// overlappings will store the overlapping between two intervals, if we iterate over this array
+// it means two intervals has already overlapped, if current also lie in overalpping range then it means 3 intervals overlapped 
     MyCalendarTwo() {
         
     }
     
     bool book(int start, int end) {
-          for(auto it:overlappings){
-            int overlapStart=it.first;
-            int overlapEnd=it.second;
-           
-            if(overlapStart<end && overlapEnd>start) {
-               return false;
-            }
-        }
+    //now before going to booking check in overlapping array whether there is overlapping intervals
+    // between 3 intervals, because overlapping array already contains the overalpping between 2 intervals 
+    for(auto it:overlappings){
+        int overlapStart=it.first;
+        int overlapEnd=it.second;
 
-        for(auto num:bookings){
-            int existingStart=num.first;
-            int existingEnd=num.second;
-           
-            if(existingStart<end && existingEnd>start) {
-               overlappings.push_back({max(existingStart,start),min(existingEnd,end)});
-            }
-        }
- cout<<endl;
-        bookings.push_back({start,end});
-        return true;
+        if(overlapStart<end && overlapEnd>start) return false;
+    }
+
+
+    for(auto num:bookings){
+// when there are elements in booking array try to find the overlapping with current interval and store in overlappings array
+int existingStart=num.first;
+int existingEnd=num.second;
+if(existingStart<end && existingEnd>start){
+    overlappings.push_back({max(existingStart,start),min(existingEnd,end)});
+}
+    }
+
+//for the first booking push directly inside the booking and for futher bookings check the above conditions
+    bookings.push_back({start,end});
+    return true;
     }
 };
 
 /**
  * Your MyCalendarTwo object will be instantiated and called as such:
  * MyCalendarTwo* obj = new MyCalendarTwo();
- * bool param_1 = obj->book(start,end);
+ * bool param_1 = obj->book(startTime,endTime);
  */
