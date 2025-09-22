c2c635bc7d473701481d2d7d801058d386f51c21
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-09-22 09:27:18 +0530

    Time: 0 ms (100%), Space: 8.2 MB (80.78%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     int hammingWeight(int n) {
+        // int count=0;
+        // while(n){
+        //     if(n&1){
+        //         count++;
+        //     }
+        //     n=n>>1;
+        // }
+        // return count;
+
+        // make a mask shift 1 to left side by 32 position
+        long long mask=1L<<32;
         int count=0;
-        while(n){
-            if(n&1){
-                count++;
-            }
-            n=n>>1;
+        while(mask){
+            if(mask & n) count++;
+            mask>>=1;//right shift the mask by 1 bit
         }
         return count;
     }
