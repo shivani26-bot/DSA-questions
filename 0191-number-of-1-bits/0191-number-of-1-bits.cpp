62577bd0ab83313653e4eb073c3fca3425b0b410
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-09-22 09:43:54 +0530

    Time: 0 ms (100%), Space: 8.3 MB (48.06%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -11,11 +11,18 @@ public:
         // return count;
 
         // make a mask shift 1 to left side by 32 position
-        long long mask=1L<<32;
-        int count=0;
-        while(mask){
-            if(mask & n) count++;
-            mask>>=1;//right shift the mask by 1 bit
+        // long long mask=1L<<32;
+        // int count=0;
+        // while(mask){
+        //     if(mask & n) count++;
+        //     mask>>=1;//right shift the mask by 1 bit
+        // }
+        // return count;
+
+int count=0;
+        while(n){
+            n=(n&(n-1));
+            count++;
         }
         return count;
     }
