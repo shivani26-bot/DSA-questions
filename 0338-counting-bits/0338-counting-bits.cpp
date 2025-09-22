e6d89bd64e374c93f5c34fdbfbc61541b48aa83b
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-09-22 10:00:05 +0530

    Time: 0 ms (100%), Space: 10.5 MB (97.12%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-int countOnes(int n){
-    int count=0;
-    while(n){
-        n=(n&(n-1));
-        count++;
-    }
-    return count;
-}
+// int countOnes(int n){
+//     int count=0;
+//     while(n){
+//         n=(n&(n-1));
+//         count++;
+//     }
+//     return count;
+// }
     vector<int> countBits(int n) {
         
-vector<int>ans;
-        for(int i=0;i<=n;i++){
-            int ones=countOnes(i);
-            ans.push_back(ones);
-        }
-        return ans;
+// vector<int>ans;
+//         for(int i=0;i<=n;i++){
+//             int ones=countOnes(i);
+//             ans.push_back(ones);
+//         }
+//         return ans;
+
+
+    //     // any number has log(x) bits, 
+    //     // timecomplexity: nlog(i)
+
+vector<int>ans(n+1,0);
+for(int i=1;i<=n;i++){
+    ans[i]=__builtin_popcount(i);
+}
+return ans;
     }
 };
