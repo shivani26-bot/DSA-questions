948a773569f370ef2718c851e16db6f30ea5f939
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-09-23 07:34:33 +0530

    Time: 0 ms (100%), Space: 21.7 MB (70.19%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,12 +1,53 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        int n=nums.size();
-        int totalSum=0;
-        for(int i=0;i<n;i++){
-            totalSum+=nums[i];
-        }
-        return (n*(n+1)/2)-totalSum;
+      int missingNumber(vector<int>& arr) {
+    
+// method1
+// sort(arr.begin(),arr.end());
+// for(int i=0;i<arr.size();i++){
+//     if(arr[i]>i){
+// return i;
+// break;
 
+//     } 
+    
+// }
+// return arr.size();
+// method2
+// int n=arr.size();
+// int totalsum=n*(n+1)/2;
+// int arrsum=0;
+// for(int i=0;i<n;i++){
+//    arrsum+=arr[i];
+// }
+// int ans=(totalsum-arrsum);
+// return ans;
+
+// method3
+// int i=0;
+// while(i<arr.size()){
+//     int currentIndex=arr[i];
+//     while(arr[i]!=arr[currentIndex] && arr[i]<arr.size()){
+//         swap(arr[i],arr[currentIndex]);
+//         if(i>=arr.size()-1) {
+             
+//           break;
+//         }
+//     }
+//      i++;
+// }
+// for(int i=0;i<arr.size();i++){
+//     if(arr[i]!=i) return i;
+// }
+// return arr.size();
+
+// method4
+   int result=arr.size();
+   
+   for(int i=0;i<arr.size();i++){
+  result^=arr[i];
+  result^=i;
+   }
+   return result;
     }
 };
