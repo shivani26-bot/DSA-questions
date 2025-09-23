669cc50c1dfcf5d5229283e26dd3974fdb578e1c
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-09-23 08:22:17 +0530

    Time: 0 ms (100%), Space: 8.2 MB (44.03%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+//      Reversing the bits means flipping the order of all 32 bits.
+// So the first bit becomes the last bit, the second becomes the second-to-last, and so on.   
+// The least significant bit should become the most significant, and vice versa.
+int res=0;
+for(int shift=31;shift>=0;shift--){
+    // find the right most bit first 
+    int bit= n & 1;
+    res=res+(bit<<shift);//shift the bit to the left by shift times
+    n=n>>1; //right shift by 1 to get the next bit
+}
+return res;
+    }
+};
