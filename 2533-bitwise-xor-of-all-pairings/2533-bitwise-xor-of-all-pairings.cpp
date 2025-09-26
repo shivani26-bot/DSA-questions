b8c5a9c95f5b5e097582d1d528f8e9d318c0b2a6
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-09-26 12:04:53 +0530

    Time: 3 ms (16.9%), Space: 64.3 MB (43.18%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
-        // int result=0;
-        // for(int i=0;i<nums1.size();i++){
-        //     for(int j=0;j<nums2.size();j++){
-        //       int currXor= nums1[i]^nums2[j];
-        //       result^=currXor;
-        //     }
-        // }
-        // return result;
-        int result=0;
+        // try xoring the elements with different sizes of array 
+        // n-even m=odd, n=odd m=even, n=odd m=odd , n=even, m=even  you will get the answer
         int n=nums1.size();
         int m=nums2.size();
+        int result=0;
         if(n%2==1 && m%2==0){
+            // if n is odd and m is even it means each element in nums1 will have even times pair hence element in n will cancel out only m is remaining 
+            // [2,1,3] [10,2,5,0]
+            // 2^10 ^ 2^2 ^ 2^5 ^ 2^0 ^ 1^10 ^ 1^2 ^ 1^5 ^ 1^0 ^ 3^10 ^ 3^2 ^ 3^5 ^ 3^0
+            for(int i=0;i<m;i++){
+                result^=nums2[i];
+            }
+        }  else if(n%2==0 && m%2==1){
+            for(int i=0;i<n;i++){
+                result^=nums1[i];
+            }
+        } else if(n%2==1 && m%2==1){
+            for(int i=0;i<n;i++){
+                result^=nums1[i];
+            }
             for(int i=0;i<m;i++){
                 result^=nums2[i];
             }
         }
-        else if(n%2==0 && m%2==1){
-             for(int i=0;i<n;i++){
-                result^=nums1[i];
-            }
-        }
-        else if(n%2==1 && m%2==1){
-             for(int i=0;i<n;i++){
-                result^=nums1[i];
-            }
-             for(int i=0;i<m;i++){
-                result^=nums2[i];
-            }
-        }
-        else{
-            return 0;
-        }
+        else return 0;
+
         return result;
     }
 };
