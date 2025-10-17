ad6ba81833ec940e3d20efa95895ce5539217eb5
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-10-17 12:08:44 +0530

    Time: 0 ms (100%), Space: 7.8 MB (64.33%) - LeetHub

--- 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.cpp
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        int matches=0;
+//         int matches=0;
+// //logn at each step n is divided by 2
+//         while(n>1){
+//             if(n%2==0){
+//                 //even teams
+//                 matches+=n/2;
+//                 n=n/2;
+//             }
+//             else{
+//                 matches+=(n-1)/2;
+//                 n=(n-1)/2+1;
+//             }
+//         }
+//         return matches;
 
-        while(n>1){
-            if(n%2==0){
-                //even teams
-                matches+=n/2;
-                n=n/2;
-            }
-            else{
-                matches+=(n-1)/2;
-                n=(n-1)/2+1;
-            }
-        }
-        return matches;
+// directly consider beginning and end of tournament 
+// there are n teams, 1 winner, it means n-1 teams will be eliminated
+// each match is played between 2 teams, one team wins one losses, each match eliminates exactly 1 team;
+// hence n-1 teams will be eliminated , there will be n-1 matches played
+return n-1;
     }
 };
