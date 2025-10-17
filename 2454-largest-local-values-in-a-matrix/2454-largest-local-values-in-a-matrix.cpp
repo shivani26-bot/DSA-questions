1b09564b57a40407d3925d82437e20f190b166d7
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-10-17 15:12:06 +0530

    Time: 3 ms (40.2%), Space: 15.7 MB (55.32%) - LeetHub

--- 2454-largest-local-values-in-a-matrix/2454-largest-local-values-in-a-matrix.cpp
+++ 2454-largest-local-values-in-a-matrix/2454-largest-local-values-in-a-matrix.cpp
@@ -4,7 +4,7 @@ public:
 
         int n=grid.size();
                 vector<vector<int>>maxLocal(n-2,vector<int>(n-2));
-                // for every cell in answer matrix, we need to go through 3x3 matrix in grid 
+    // for every cell in answer matrix, we need to go through 3x3 matrix in grid 
         for(int i=0;i<n-2;i++){
             for(int j=0;j<n-2;j++){
                 int maxm=INT_MIN;
