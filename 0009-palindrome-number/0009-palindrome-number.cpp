647e28dfc11ec6bc4673071c5c480846afe32f2f
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-10-18 12:37:57 +0530

    Time: 0 ms (100%), Space: 8.6 MB (67.73%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string original=to_string(x);
-        string x_= to_string(x);
-        reverse(x_.begin(),x_.end());
-        return x_==original;
+        // string original=to_string(x);
+        // string x_= to_string(x);
+        // reverse(x_.begin(),x_.end());
+        // return x_==original;
+
+        // if number is less than 0 it can never be palindrome because - will come in end 
+        if(x<0) return false;
+
+        // only run a logic for x>0 
+        // find the reverse of a number 
+        int num=x;
+        long long rev=0;
+        while(num){
+            rev=rev*10+num%10;
+            num=num/10;
+        }
+        return x==rev;
     }
 };
