958e223ce1edc538a9d273200375f117d17b3f6b
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-10-19 12:54:26 +0530

    Time: 8 ms (40.82%), Space: 12.3 MB (63.81%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int romanToInt(string s) {
+//         when a smaller value comes before a larger one,
+// it means subtraction.
+// Example:
+// IV = 5 - 1 = 4
+// IX = 10 - 1 = 9
+// XL = 50 - 10 = 40
+// XC = 100 - 10 = 90
+
+// You process the Roman string left to right:
+// Look at the current symbol and the next one.
+// If current ≥ next, just add the current value.
+// If current < next, then subtract the current value.
+
+// Why?
+// Because:
+// A smaller value before a larger one means we’ve “looked ahead” to a subtractive pair (like IV or IX).
+// A larger or equal value means we can safely add it (like VI or XI).
         unordered_map<char,int>mp;
         mp['I']=1;
         mp['V']=5;
@@ -21,7 +38,7 @@ else{
     //if next number is greater than current number then subtract 
     sum-=mp[s[i]];
 }
-        }
+}
 return sum;
         }
     
