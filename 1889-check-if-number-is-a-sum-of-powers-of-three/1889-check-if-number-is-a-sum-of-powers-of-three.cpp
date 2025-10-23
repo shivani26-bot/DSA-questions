f8b9e1a9e9802a1217685b9341e8b5d6190c224d
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-10-23 10:14:35 +0530

    Time: 0 ms (100%), Space: 7.9 MB (41.53%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -23,19 +23,58 @@ public:
         // 21-3^2=21-9=12 //if still greater than current power of 3 ie 3^2 return false
         // 12>=3^2 but we cannot use same power again
 // find the max power 
-int p=0;
-// untill the power of 3 < n increment p 
-while(pow(3,p)<n){
-    p++;
+// int p=0;
+// // untill the power of 3 < n increment p 
+// while(pow(3,p)<n){
+//     p++;
+// }
+//  while(n>0){
+//     if(n>=pow(3,p)){
+//         n=n-pow(3,p);
+//     }
+//     // even after subtracting n is > current power of 3 return false;
+//     if(n>=pow(3,p)) return false;
+//     p--;
+//  }
+// return true;
+
+// binary representation of 15
+// divide the number by 2 continuously and write the remainder as ans
+// 2|15|1
+// 2|7|1
+// 2|3|1
+// 2|1|1
+//   0
+// 15= 0 1 1 1 1
+// we can convert binary to decimal
+// (0*2^4) + (1*2^3) +(1*2^2) +(1*2^1) + (1*2^0)
+// n= (dk*2^k) +  (dk-1*2^k-1) + ..... + (d0*2^0) 
+// how many time a power will be used, 2^3 will be used 1 time
+// similarly ternary representation, divide by 3
+// n=12
+// 3|12|0
+// 3|4|1
+// 3|1|1
+//   0
+// 13=0110
+// n= (dk*3^k) +  (dk-1*3^k-1) + ..... + (d0*2^0) 
+// 12= (0*3^3) + (1*3^2) +(1*3^1) +(0*3^0) 
+
+// n=21
+// 3|21|0
+// 3|7|1
+// 3|2|2
+//   0
+// 21=0210
+// 21=(0*3^3) + (2*3^2) +(1*3^1) +(0*3^0) 
+// here 3^2 is coming 2 times hence ans is false
+// if we have other than 0 or 1 it is false
+// x%3=0/1/2 if its 2 then false
+
+while(n>0){
+    if(n%3==2) return false;
+    n=n/3;
 }
- while(n>0){
-    if(n>=pow(3,p)){
-        n=n-pow(3,p);
-    }
-    // even after subtracting n is > current power of 3 return false;
-    if(n>=pow(3,p)) return false;
-    p--;
- }
 return true;
     }
 };
