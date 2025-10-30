c806f0713235c2401838bc079898b94934ce2bdc
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-10-30 07:30:30 +0530

    Time: 0 ms (100%), Space: 52.2 MB (31.83%) - LeetHub

--- 2756-buy-two-chocolates/2756-buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/2756-buy-two-chocolates.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
     
-        // try to find first minimum and second minimum o(n)
+        // try to find  minimum and second minimum o(n)
 int minPrice=INT_MAX;
 int secMinPrice=INT_MAX;
         for(int i=0;i<prices.size();i++){
@@ -15,7 +15,9 @@ int secMinPrice=INT_MAX;
             secMinPrice=min(secMinPrice,prices[i]);
           }
         }
+        // if sum of min and sec min is > than money then return the money it's not possible to find exactly two chocolates
 if(minPrice+secMinPrice>money) return money;
+// otherwise return the leftover money
 return money-(minPrice+secMinPrice);
 
     }
