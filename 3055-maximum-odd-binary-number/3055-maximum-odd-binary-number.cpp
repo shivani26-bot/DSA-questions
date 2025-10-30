c2f02e4f2a26ba0cd8b1b62a4c8639b7e72fcd73
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-10-30 08:45:09 +0530

    Time: 4 ms (10.67%), Space: 9.2 MB (28.4%) - LeetHub

---
+++ 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        // think greedily to make the maximum, start filling 1 from left hand side lsb, and to make odd , just put 1 at n-th bit, because all other are power of 2 ie even only 2^0 is 1 which can make number as odd
+        int n=s.length();
+    //   char ans[n];
+    //    memset(ans, '0', sizeof(char) * n);
+    vector<char>ans(n,'0');
+    //    count number of 1's in string 
+      int count1=0;
+      for(int i=0;i<n;i++){
+          if(s[i]=='1') count1++;
+      }
+cout<<count1<<endl;
+      if(count1>=1) {
+          ans[n-1]='1';
+          count1--;
+      }
+      int i=0;
+      while(count1!=0&& i<n){
+          ans[i]='1';
+          count1--;
+          i++;
+      }
+    //   for(int i=0;i<n;i++) cout<<ans[i]<<" ";
+      string result;
+    //   for(int i=n-1;i>=0;i--){
+    //       result.push_back(ans[i]);
+    //   }
+     for(int i=0;i<n;i++){
+          result.push_back(ans[i]);
+      }
+    //  reverse(result.begin(),result.end());
+      return result;
+
+    }
+};
