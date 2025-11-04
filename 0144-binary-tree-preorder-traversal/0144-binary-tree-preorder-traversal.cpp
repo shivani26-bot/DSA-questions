9028ad259187cfaf76d02d395939c74cfe36e593
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-11-04 21:32:29 +0530

    Time: 0 ms (100%), Space: 11 MB (15.14%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -15,6 +15,12 @@ public:
 // Each node is visited exactly once.
 // Each recursive call processes one node and makes two recursive calls (left and right).
 // Since there are N nodes in the tree, the function runs in O(N) time.
+// space: recursion stack depth depends on height of tree
+// If the tree is balanced, height = O(log n).
+// If the tree is skewed (like a linked list), height = O(n).
+// Total Space (including output) O(n)
+// space excluding output o(h) : Recursion stack — proportional to the tree’s height.
+// Because you store all node values in ans.
 void preorder(TreeNode* root, vector<int>&ans){
   if(!root) return ;
    ans.push_back(root->val);
