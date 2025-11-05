ba47d7adadeef87325b7df1aa565877548d3a6de
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-11-05 08:30:05 +0530

    Time: 0 ms (100%), Space: 13.5 MB (38.68%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -10,11 +10,13 @@
  */
 class Solution {
 public:
+// time: each node is visited once for each node o(1) operation is done, hence total tc: o(n)
+// space: o(1) no recursion here
     ListNode* reverseList(ListNode* head) {
         if(!head || !head->next) return head;
         ListNode* prev=NULL;
         ListNode* curr=head;
-        ListNode* nextNode=head;
+        ListNode* nextNode=head->next;
         while(nextNode){
             nextNode=curr->next;
             curr->next=prev;
