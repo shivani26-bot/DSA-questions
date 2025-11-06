b2a50db34c68f181194d65428cfd1cbc16187d6f
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-11-06 08:06:53 +0530

    Time: 3 ms (61.08%), Space: 23 MB (22.75%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -12,6 +12,11 @@
 class Solution {
 public:
 // to make a height balanced tree, always split the array into half, the mid element will be root, similarly repeat for left and right subarray
+// time: o(n)
+// each element in array is visited only once to create a node 
+// splitting array take o(1)
+// space: no extra space used , o(recursion depth)
+// balanced bst o(logn)
 TreeNode* makeBST(int s, int e, vector<int>&nums){
     if(s>e) return NULL;
     int mid=(s+e)/2;
