484f8b50fcf2d00b0fc2679bf4840a1cd2013766
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-11-08 10:45:03 +0530

    Time: 27 ms (17.72%), Space: 56.4 MB (38.19%) - LeetHub

--- 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.cpp
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.cpp
@@ -9,16 +9,80 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+ class Solution {
 public:
-  
     string tree2str(TreeNode* root) {
-        if(!root) return "";
-    string ans=to_string(root->val);
-    if(root->left || root->right){
-        ans+= "("+ tree2str(root->left)+")";
-    }
-    if(root->right) ans+= "("+tree2str(root->right)+")";
-    return ans;
+        
+        string ans = to_string(root->val);
+		if (root->left) //left side check
+			ans += "(" + tree2str(root->left) + ")";
+		if (root->right) { //right side check
+			if (!root->left) ans += "()"; //left side not present, but right side present
+			ans += "(" + tree2str(root->right) + ")"; 
+		}
+		return ans;
     }
 };
+//  class Solution {
+// public:
+  
+//     string tree2str(TreeNode* root) {
+//         if(!root) return "";
+//     string ans=to_string(root->val);
+//     if(root->left || root->right){
+//         ans+= "("+ tree2str(root->left)+")";
+//     }
+//     if(root->right) ans+= "("+tree2str(root->right)+")";
+//     return ans;
+//     }
+// };
+// class Solution {
+// public:
+  
+//     string tree2str(TreeNode* root) {
+//      if (!root) {
+//         return "";
+//     }
+
+//     string result = to_string(root->val);
+
+//     if (root->left || root->right) {
+//         result += "(" + tree2str(root->left) + ")";
+//     }
+
+//     if (root->right) {
+//         result += "(" + tree2str(root->right) + ")";
+//     }
+
+//     return result;
+//     }
+// };
+// class Solution {
+// public:
+// void dfs(TreeNode* root, string &ans){
+//     ans+=to_string(root->val);
+//     if(!root) return;
+// if(!root->left && !root->right) return;
+
+// if(root->left){
+//     ans+='(';
+//     dfs(root->left,ans);
+//     ans+=')';
+// }
+// if(!root->left && root->right) {
+//     ans+='(';
+//     ans+=')';
+// }
+// if(root->right){
+//      ans+='(';
+//     dfs(root->right,ans);
+//     ans+=')';
+// }
+
+// }
+//     string tree2str(TreeNode* root) {
+//         string ans;
+//         dfs(root,ans);
+//         return ans;
+//     }
+// };
