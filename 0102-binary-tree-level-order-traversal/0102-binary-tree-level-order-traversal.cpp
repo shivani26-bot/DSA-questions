9c99c5f6e850399b9165ea63fbbb0842079dba27
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-11-09 08:07:36 +0530

    Time: 0 ms (100%), Space: 17 MB (70.64%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -11,27 +11,35 @@
  */
 class Solution {
 public:
+// time: o(n)
+// each node is pushed/popped once in/from queue, constnat work per node 
+// space: o(w+n)
+// w= in worst case queue holds all the nodes from widest level of tree
+// In the worst case (a full binary tree), W ≈ N/2 → O(N).
+// In a balanced tree, W ≈ O(N/2) but still O(N) asymptotically.
+// for ans array n  space, storing all nodes in tree 
     vector<vector<int>> levelOrder(TreeNode* root) {
-            queue<TreeNode*> q;
-            q.push(root);
-                 vector<vector<int>>finalResult;
-            while(!q.empty()){
-                int size=q.size();
-           vector<int>result;
-                for(int i=0;i<size;i++){
-                         TreeNode* curr= q.front();
-                         q.pop();
-                          if (curr) {
-                         result.push_back(curr->val);
-                         if(curr->left) q.push(curr->left);
-                         if(curr->right) q.push(curr->right);
-                          }
+        if(!root) return {};
+        vector<vector<int>>ans;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int size= q.size();
+            vector<int>temp;
+            while(size--){
+                TreeNode* node= q.front();
+                                temp.push_back(node->val);
+                q.pop();
+
+                if(node->left){
+                   q.push(node->left);   
                 }
-                if (!result.empty()) {
-                finalResult.push_back(result);
+                  if(node->right){
+                    q.push(node->right);                 
                 }
             }
-
-         return finalResult;
+            ans.push_back(temp);
+        }
+        return ans;
     }
 };
