d5c9cb8cf8a68ae7af99f6565661347f4bf578b2
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-11-14 08:58:13 +0530

    Time: 0 ms (100%), Space: 21.8 MB (75.21%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -19,38 +19,73 @@ public:
 //     ans.push_back(root->val);
 //     traverse(root->right);
 // }
-void traverse(TreeNode* root,long &prev,bool & ans){
-    if(!root) return;
-    traverse(root->left,prev,ans);
-    if(prev<root->val){
-        prev=root->val;
-    }
-    else{
-        ans=false;
-        return;
-    }
-    traverse(root->right,prev,ans);
-}
-    bool isValidBST(TreeNode* root) {
-        // 1st approach 
-        // traverse(root);
-        // bool   isValid;
-        // for(int i=0;i<ans.size()-1;i++){
-        //     if(ans[i]<ans[i+1]){
-        //        isValid=true;
-        //        continue;
-        //     }
-        //     else{
-        //         isValid=false;
-        //         break;
-        //     }
-        // }
-        // return isValid;
+// void traverse(TreeNode* root,long &prev,bool & ans){
+//     if(!root) return;
+//     traverse(root->left,prev,ans);
+//     if(prev<root->val){
+//         prev=root->val;
+//     }
+//     else{
+//         ans=false;
+//         return;
+//     }
+//     traverse(root->right,prev,ans);
+// }
+    // bool isValidBST(TreeNode* root) {
+    //     // 1st approach 
+    //     // traverse(root);
+    //     // bool   isValid;
+    //     // for(int i=0;i<ans.size()-1;i++){
+    //     //     if(ans[i]<ans[i+1]){
+    //     //        isValid=true;
+    //     //        continue;
+    //     //     }
+    //     //     else{
+    //     //         isValid=false;
+    //     //         break;
+    //     //     }
+    //     // }
+    //     // return isValid;
       
-      //2nd approach
-      long prev=LONG_MIN;
-      bool isValid =true;
-      traverse(root,prev,isValid);
-      return isValid;
+    //   //2nd approach
+    //   long prev=LONG_MIN;
+    //   bool isValid =true;
+    //   traverse(root,prev,isValid);
+    //   return isValid;
+    // }
+
+    // bool isValidBST(TreeNode* root) {
+    //     // 1st approach 
+    //     traverse(root);
+ 
+    //     for(int i=0;i<ans.size()-1;i++){
+    //         if(ans[i]>=ans[i+1]){
+    //            return false;
+    //         }
+          
+    //     }
+    //     return true;
+      
+   
+    // }
+
+
+    // 2nd approach 
+    typedef long long ll;
+    bool isBst(TreeNode* root, ll min, ll max){
+        // if we reach the end node then return true; 
+        if(!root) return true;
+        // for every node check whether it lies in min max range or not 
+        if(root->val<=min || root->val>=max) return false;
+
+// when we move to the left the current node should be max value because all the nodes in the left side must be smaller than it and when we move to the right the current node should be min value because all the nodes in the right side must be larger than it 
+        return isBst(root->left, min, root->val) && isBst(root->right, root->val, max);
+    }
+
+     bool isValidBST(TreeNode* root) {
+// we can maintain a min , max range for every node and when we traverse to left or right, we can
+        // check for every node whether it lies within the range or not, if both the sides return true, then return true to parent node 
+      
+   return isBst(root, LLONG_MIN, LLONG_MAX);
     }
 };
