2c86cadebc8175e8f6020f7872835e06a40473d3
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-11-14 08:59:54 +0530

    Time: 1 ms (62.36%), Space: 56.3 MB (99.7%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -12,20 +12,35 @@
  */
 var isValidBST = function(root) {
 
-    const ans=[];
-function traverse(root){
-      if(!root) return;
-    traverse(root.left);
-    ans.push(root.val);
-    traverse(root.right);
-}
-    traverse(root);
+//     const ans=[];
+// function traverse(root){
+//       if(!root) return;
+//     traverse(root.left);
+//     ans.push(root.val);
+//     traverse(root.right);
+// }
+//     traverse(root);
  
-        for(let i=0;i<ans.length-1;i++){
-            if(ans[i]>=ans[i+1]){
-               return false;
-            }
+//         for(let i=0;i<ans.length-1;i++){
+//             if(ans[i]>=ans[i+1]){
+//                return false;
+//             }
           
-        }
-        return true;
+//         }
+//         return true;
+
+
+
+
+   function isBst(root, min, max){
+        // if we reach the end node then return true; 
+        if(!root) return true;
+        // for every node check whether it lies in min max range or not 
+        if(root.val<=min || root.val>=max) return false;
+
+// when we move to the left the current node should be max value because all the nodes in the left side must be smaller than it and when we move to the right the current node should be min value because all the nodes in the right side must be larger than it 
+        return isBst(root.left, min, root.val) && isBst(root.right, root.val, max);
+    }
+
+ return isBst(root, -Infinity, Infinity);
 };
