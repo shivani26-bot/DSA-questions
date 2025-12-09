abb9abba8488d1dbb8e13201f253427e599f2e18
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2025-12-09 09:43:25 +0530

    Time: 59 ms (81.71%), Space: 59 MB (55.67%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -0,0 +1,32 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+     let islandCount=0;
+      const n=grid.length;
+   const m=grid[0].length;
+
+     function dfs( grid,i, j, visited){
+if(i<0 || i>=n || j<0 || j>=m ||  visited[i][j]=='1' || grid[i][j]=='0')
+return;
+
+visited[i][j]='1';
+
+    dfs(grid,i+1,j,visited);
+    dfs(grid,i-1,j,visited);
+    dfs(grid,i,j+1,visited);
+    dfs(grid,i,j-1,visited);
+
+}
+     const visited= Array.from({length:n},()=>Array.from({length:m},()=>'0'));
+      for(let i=0;i<n;i++){
+        for(let  j=0;j<m;j++){
+            if(grid[i][j]=='1' && visited[i][j]=='0'){
+                islandCount++;
+                dfs(grid,i,j,visited);
+            }
+        }
+      }
+return islandCount;
+};
