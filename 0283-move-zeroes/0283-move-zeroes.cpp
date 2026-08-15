ab081e916b8557fa11010a53bb595d474cf88b70
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2026-08-15 18:11:36 +0530

    Time: 0 ms (100%), Space: 23.9 MB (56.64%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        // int i=0,j=0;
+        // int n=nums.size();
+        // while(j<n){
+        //  if(nums[j]!=0){
+        //     swap(nums[i],nums[j]);
+        //     i++;
+        //  }
+        //  j++;
+        // }
+
+
+              int i=0,j=0;
         int n=nums.size();
-        int i=0,j=0;
         while(j<n){
-            if(nums[j]!=0){
-                swap(nums[i],nums[j]);
-                i++;
+         if(nums[j]!=0){
+            if(j!=i){
+            swap(nums[i],nums[j]);
             }
-            j++;
+            i++;
+         }
+         j++;
         }
     }
 };
-
-// move zeros to the end, maintain two pointers i=0, j=0, move i and j such that i always point to 0 and j to a  nonzero element
