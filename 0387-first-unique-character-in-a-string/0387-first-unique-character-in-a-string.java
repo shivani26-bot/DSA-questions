d981f88afa1079782c0042ce8b1464cf32394bfa
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2026-09-15 20:45:15 +0530

    Time: 46 ms (6.51%), Space: 47.3 MB (13.45%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -2,16 +2,29 @@ class Solution {
     public int firstUniqChar(String s) {
         int[] freq = new int[26];
 
-        for(int i=0;i<s.length();i++){
-            freq[s.charAt(i)-'a']++;
-        }
+//         for(int i=0;i<s.length();i++){
+//             freq[s.charAt(i)-'a']++;
+//         }
 
- for(int i=0;i<s.length();i++){
-             if(freq[s.charAt(i)-'a']==1) return i;
-        }
+//  for(int i=0;i<s.length();i++){
+//              if(freq[s.charAt(i)-'a']==1) return i;
+//         }
         
 
-        return -1;
-        
+//         return -1;
+
+Map<Character,Integer> count= new HashMap<>();
+Deque<Integer> ele= new ArrayDeque<>();
+for(int i=0;i<s.length();i++){
+    Character ch= s.charAt(i);
+    count.merge(ch, 1, Integer::sum);
+
+    ele.addLast(i);
+    while(!ele.isEmpty() &&  count.get(s.charAt(ele.peekFirst())) > 1 ){
+        ele.pollFirst();
+    }
+}
+
+ return ele.isEmpty() ? -1 : ele.peekFirst();
     }
 }
