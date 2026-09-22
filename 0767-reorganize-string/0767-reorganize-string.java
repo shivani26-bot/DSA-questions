9d211da3d961b806d14e41c4ec9a7ec646261a8f
Shivani Kumari <84676101+shivani26-bot@users.noreply.github.com>
2026-09-22 12:52:21 +0530

    Time: 1 ms (96.54%), Space: 43.1 MB (40.16%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,35 +1,73 @@
 class Solution {
     public String reorganizeString(String s) {
-        int[] freq = new int[26];
+//         int[] freq = new int[26];
+// for(char c: s.toCharArray()){
+//     freq[c-'a']++;
+// }
+
+// int n= s.length();
+// for(int f: freq){
+//     if(f>(n+1)/2) return "";
+// }
+
+// List<int[]> sorted = new ArrayList<>();
+// for(int i=0;i<26;i++){
+//     if(freq[i]>0){
+//         //for a list with {freq, char}
+//         sorted.add(new int[]{freq[i],i});
+//     }
+// }
+
+// sorted.sort((a,b) -> b[0]-a[0]);
+
+// char[] result = new char[n];
+// int i=0;
+// for(int[] entry: sorted){
+//     int count = entry[0];
+//     char c = (char) (entry[1]+'a');
+//     while(count>0){
+//         result[i]= c;
+//         i+=2;
+//         count--;
+//         if(i>=n) i=1;
+//     }
+
+// }
+// return new String(result);
+
+int[] freq = new int[26];
+int n= s.length();
+int maxFreq= 0;
+char maxFreqChar = 'a';
+//o(n)
 for(char c: s.toCharArray()){
     freq[c-'a']++;
-}
-
-int n= s.length();
-for(int f: freq){
-    if(f>(n+1)/2) return "";
-}
-
-List<int[]> sorted = new ArrayList<>();
-for(int i=0;i<26;i++){
-    if(freq[i]>0){
-        //for a list with {freq, char}
-        sorted.add(new int[]{freq[i],i});
+    if(freq[c-'a'] > maxFreq){
+        maxFreq= freq[c-'a'];
+        maxFreqChar= c;
     }
+
+    if(freq[c-'a'] > (n+1)/2) return "";
 }
 
-sorted.sort((a,b) -> b[0]-a[0]);
 
 char[] result = new char[n];
 int i=0;
-for(int[] entry: sorted){
-    int count = entry[0];
-    char c = (char) (entry[1]+'a');
-    while(count>0){
-        result[i]= c;
-        i+=2;
-        count--;
+
+while(freq[maxFreqChar-'a']>0){ //o(n)
+    result[i] = maxFreqChar;
+    i+=2;
+    freq[maxFreqChar-'a']--;
+}
+
+// o(26 + n)
+for(char ch='a' ; ch<='z'; ch++){ //26 times, ie o(1)
+    while(freq[ch-'a']>0){ //o(n)
         if(i>=n) i=1;
+
+           result[i] = ch;
+           i+=2;
+           freq[ch-'a']--;
     }
 
 }
